3eb675dc8774afabaafa60222ef2d24c8e4d2439
Robbert Broersma <git@robbertbroersma.nl>
2023-12-03 17:35:23 +0100

    style: add Status badge component to Storybook #41

---
+++ packages/storybook/src/css-utrecht-status-badge.stories.tsx
@@ -0,0 +1,102 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { StatusBadge } from '@utrecht/component-library-react/dist/css-module';
+import readme from './status-badge.md?raw';
+import '@gemeente-rotterdam/components-css/button/index.scss';
+
+const meta = {
+  title: 'CSS Component/Status badge',
+  id: 'css-utrecht-status-badge',
+  component: StatusBadge,
+  argTypes: {
+    children: {
+      name: 'Content',
+      type: {
+        name: 'string',
+        required: true,
+      },
+      defaultValue: '',
+    },
+    className: {
+      name: 'Appearance',
+      control: 'select',
+      options: {
+        'Light blue': 'utrecht-status-badge--rods-light-yellow',
+        Blue: 'utrecht-status-badge--rods-blue',
+        'Dark blue': 'utrecht-status-badge--rods-dark-blue',
+        'Green blue': 'utrecht-status-badge--rods-green-blue',
+        Red: 'utrecht-status-badge--rods-red',
+        'Dark yellow': 'utrecht-status-badge--rods-dark-yellow',
+      },
+    },
+  },
+  args: {
+    children: '',
+    status: 'fixme',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/?node-id=6359-9671',
+    },
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof StatusBadge>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const LightBlue: Story = {
+  name: 'Light blue',
+  args: {
+    children: 'label',
+    className: 'utrecht-status-badge--rods-light-blue',
+  },
+};
+
+export const Blue: Story = {
+  name: 'Blue',
+  args: {
+    children: 'label',
+    className: 'utrecht-status-badge--rods-blue',
+  },
+};
+
+export const DarkBlue: Story = {
+  name: 'Dark blue',
+  args: {
+    children: 'label',
+    className: 'utrecht-status-badge--rods-dark-blue',
+  },
+};
+
+export const GreenBlue: Story = {
+  name: 'Green blue',
+  args: {
+    children: 'label',
+    className: 'utrecht-status-badge--rods-green-blue',
+  },
+};
+
+export const Red: Story = {
+  name: 'Red',
+  args: {
+    children: 'label',
+    className: 'utrecht-status-badge--rods-red',
+  },
+};
+
+export const DarkYellow: Story = {
+  name: 'Dark yellow',
+  args: {
+    children: 'label',
+    className: 'utrecht-status-badge--rods-dark-yellow',
+  },
+};
