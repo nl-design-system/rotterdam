fd33a6e5ad81817426e5e739ab29b175b31fa623
Robbert Broersma <git@robbertbroersma.nl>
2023-12-03 19:54:06 +0100

    style: add Unordered list component to Storybook #50

---
+++ packages/storybook/src/css-utrecht-unordered-list.stories.tsx
@@ -0,0 +1,57 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { UnorderedList, UnorderedListItem } from '@utrecht/component-library-react/dist/css-module';
+import readme from './unordered-list.md?raw';
+
+const meta = {
+  title: 'CSS Component/Unordered list',
+  id: 'css-utrecht-unordered-list',
+  component: UnorderedList,
+  argTypes: {
+    children: {
+      name: 'Content',
+      type: {
+        name: 'string',
+        required: true,
+      },
+      defaultValue: '',
+    },
+  },
+  args: {
+    children: '',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/?node-id=490-17441',
+    },
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof UnorderedList>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  name: 'Unordered list',
+  args: {
+    children: [
+      <UnorderedListItem>
+        In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)
+      </UnorderedListItem>,
+      <UnorderedListItem>
+        In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)
+      </UnorderedListItem>,
+      <UnorderedListItem>
+        In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)
+      </UnorderedListItem>,
+    ],
+  },
+};
