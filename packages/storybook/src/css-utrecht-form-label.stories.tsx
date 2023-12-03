a1f302769a64d280b6f8dd01bbb095c641e19d8c
Robbert Broersma <git@robbertbroersma.nl>
2023-12-03 17:00:45 +0100

    docs: add Form label component in Storybook #22

---
+++ packages/storybook/src/css-utrecht-form-label.stories.tsx
@@ -0,0 +1,47 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { FormLabel } from '@utrecht/component-library-react/dist/css-module';
+import readme from './form-label.md?raw';
+
+const meta = {
+  title: 'CSS Component/Form label',
+  id: 'css-utrecht-form-label',
+  component: FormLabel,
+  argTypes: {
+    children: {
+      name: 'Content',
+      type: {
+        name: 'string',
+        required: true,
+      },
+      defaultValue: '',
+    },
+  },
+  args: {
+    children: '',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/?node-id=435-13376',
+    },
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof FormLabel>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  name: 'Form label',
+  args: {
+    children: 'Label',
+  },
+};
