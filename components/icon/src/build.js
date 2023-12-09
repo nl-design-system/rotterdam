384b23666e63e6f57b6bc7aa7744caa1dfc31d0c
Bryan de Jong <bryandejong99@gmail.com>
2023-12-09 12:16:09 +0000

    feat: add icon component

---
+++ components/icon/src/build.js
@@ -0,0 +1,97 @@
+/* eslint-env node */
+const lodash = require('lodash');
+const argv = require('minimist')(process.argv.slice(2), {
+  string: ['prefix', 'path'],
+});
+const { readdir, readFile, writeFile, mkdir } = require('node:fs/promises');
+const path = require('path');
+const { component, test, generateIconsName, style, iconContainerComponent } = require('./component_templates.js');
+
+const { kebabCase } = lodash;
+const componentPrefix = `${argv.prefix}-`;
+const componentsPath = argv.path;
+const iconRapperComponent = `${componentPrefix}container`;
+const svgPath = path.resolve(process.cwd(), 'tmp/optimized-svgs');
+
+const readSVGDir = async () => {
+  let files;
+  try {
+    files = await readdir(svgPath);
+    return files;
+  } catch (error) {
+    console.error(error);
+  }
+  return files;
+};
+
+const getIconData = async () => {
+  let svgData;
+
+  const files = await readSVGDir();
+  try {
+    if (files && files.length > 0) {
+      svgData = await Promise.all(
+        files.map((file) => ({
+          id: `${componentPrefix}${kebabCase(path.parse(file).name)}`,
+          src: file,
+        })),
+      );
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  return svgData;
+};
+
+const writeComponentFile = async () => {
+  try {
+    const files = await readSVGDir();
+    if (files && files.length > 0) {
+      for (const file of files) {
+        const fileBasename = path.parse(file).name;
+        const fileName = `${componentPrefix}${kebabCase(fileBasename)}`;
+        const formattedName = kebabCase(fileName);
+        const svg = await readFile(`${svgPath}/${file}`, 'utf-8');
+
+        await mkdir(`${componentsPath}/${formattedName}`, { recursive: true });
+        await writeFile(
+          `${componentsPath}/${formattedName}/${formattedName}.stencil.tsx`,
+          component(fileName, svg, iconRapperComponent),
+          'utf-8',
+        );
+        await writeFile(`${componentsPath}/${formattedName}/${formattedName}.spec.tsx`, test(fileName), 'utf-8');
+      }
+    } else {
+      console.log('There is no SVG files');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+writeComponentFile();
+
+const generateIconsNameFile = async () => {
+  const data = await getIconData();
+  await mkdir('dist', { recursive: true });
+  await writeFile(path.resolve(process.cwd(), 'dist/index.json'), generateIconsName(data), 'utf-8');
+};
+generateIconsNameFile();
+
+const createIconWrapperComponent = async () => {
+  const dir = `${componentsPath}/${iconRapperComponent}`;
+  const cssValue = `.${iconRapperComponent} {
+    display: inline-block;
+    color: var(--${componentPrefix}color);
+    width: var(--${componentPrefix}size);
+    height: var(--${componentPrefix}size);
+  }`;
+  await mkdir(dir, { recursive: true });
+  await writeFile(
+    `${dir}/${iconRapperComponent}.stencil.tsx`,
+    iconContainerComponent(iconRapperComponent, '<slot/>'),
+    'utf-8',
+  );
+  await writeFile(`${dir}/${iconRapperComponent}.spec.tsx`, test(iconRapperComponent), 'utf-8');
+  await writeFile(`${dir}/${iconRapperComponent}.scss`, style(cssValue), 'utf-8');
+};
+createIconWrapperComponent();
---
+++ components/icon/src/component_templates.js
@@ -0,0 +1,68 @@
+/* eslint-disable no-undef */
+const lodash = require('lodash');
+const { camelCase, kebabCase, upperFirst } = lodash;
+
+/* eslint-disable-next-line no-unused-vars */
+exports.component = (name, children, container) => {
+  const ComponentName = upperFirst(camelCase(name));
+  const webComponentName = kebabCase(name);
+
+  return `import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: '${webComponentName}',
+  styleUrl: "../../icon/generated.scss",
+  shadow: true,
+})
+export class ${ComponentName} {
+  render() {
+    return (
+      ${children}
+    );
+  }
+}
+`;
+};
+
+exports.iconContainerComponent = (name, children) => {
+  const ComponentName = upperFirst(camelCase(name));
+  const webComponentName = kebabCase(name);
+
+  return `import { Component, h } from '@stencil/core';
+
+  @Component({
+    tag: '${webComponentName}',
+    styleUrl: './${webComponentName}.scss',
+    shadow: true,
+  })
+  export class ${ComponentName} {
+    render() {
+      return (
+        <div class='${webComponentName}'>
+          ${children}
+        </div>
+      );
+    }
+  }
+  `;
+};
+
+exports.test = (name) => {
+  const ComponentName = upperFirst(camelCase(name));
+  const webComponentName = kebabCase(name);
+  return `/* eslint-env jest */
+import { ${ComponentName} } from './${webComponentName}.stencil';
+
+describe('${ComponentName}', () => {
+  it('builds', () => {
+    expect(new ${ComponentName}()).toBeTruthy();
+  });
+});
+`;
+};
+
+exports.generateIconsName = (iconsNames) => {
+  return JSON.stringify(iconsNames, null, '  ');
+};
+
+exports.style = (value) => value;
