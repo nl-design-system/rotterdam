a4ad1b2942c3e0029a46ad6c787a85ca961a9517
Robbert Broersma <git@robbertbroersma.nl>
2023-12-10 20:19:57 +0100

    ci: avoid upgrading to stylelint 16 for now

--- .ncurc.major.js
+++ .ncurc.major.js
@@ -1,5 +1,5 @@
 const minorConfig = require('./.ncurc.minor');
 
 module.exports = {
-  reject: ['@mdx-js/react', ...minorConfig.reject],
+  reject: ['@mdx-js/react', 'stylelint', ...minorConfig.reject],
 };
