a4beb9ba6e36685ae7fb09a1d1fd612b2c157b18
Bryan de Jong <bryandejong99@gmail.com>
2023-12-12 16:27:10 +0000

    feat: add sidenav to page-mijn-loket

--- packages/storybook/src/template/page-mijn-loket.stories.tsx
+++ packages/storybook/src/template/page-mijn-loket.stories.tsx
@@ -1,7 +1,19 @@
 /* @license CC0-1.0 */
 
+import { Sidenav, SidenavItem, SidenavLink, SidenavList } from '@gemeente-denhaag/sidenav';
+import {
+  RodsIconBox,
+  RodsIconCoins,
+  RodsIconInbox,
+  RodsIconMoney2,
+  RodsIconOverview,
+  RodsIconParking,
+  RodsIconSummary,
+  RodsIconUser,
+} from '@gemeente-rotterdam/web-components-react';
 import { Meta, StoryObj } from '@storybook/react';
 import '@gemeente-rotterdam/components-css/grid/index.scss';
+import { BadgeCounter } from '@utrecht/component-library-react/dist/css-module';
 
 const meta = {
   title: 'Template/Mijn Loket',
@@ -17,26 +29,68 @@ export const Default: Story = {
     <div {...args} style={{ containerType: 'inline-size' }}>
       <div className={'rods-grid'}>
         <div className={'rods-grid__full-width'} style={{ backgroundColor: 'cadetblue' }}>
-          <h1>rods-grid</h1>
           <p>Full width segment.</p>
         </div>
-        <div className={'rods-grid__one-third'} style={{ backgroundColor: 'lightsalmon' }}>
-          <h2>One third</h2>
+        <div className={'rods-grid__one-fourth'} style={{ backgroundColor: 'lightsalmon' }}>
+          <Sidenav>
+            <SidenavList>
+              <SidenavItem>
+                <SidenavLink current href="/#">
+                  <RodsIconOverview /> Overzicht
+                </SidenavLink>
+              </SidenavItem>
+              <SidenavItem>
+                <SidenavLink href="/#">
+                  <RodsIconInbox />
+                  Mijn berichten
+                  <BadgeCounter>2</BadgeCounter>
+                </SidenavLink>
+              </SidenavItem>
+              <SidenavItem>
+                <SidenavLink href="/#">
+                  <RodsIconBox /> Mijn lopende zaken
+                </SidenavLink>
+              </SidenavItem>
+              <SidenavItem>
+                <SidenavLink href="/#">
+                  <RodsIconSummary /> Zelf regelen
+                </SidenavLink>
+              </SidenavItem>
+            </SidenavList>
+            <SidenavList>
+              <SidenavItem>
+                <SidenavLink href="/#">
+                  <RodsIconCoins /> Belastingen
+                </SidenavLink>
+              </SidenavItem>
+              <SidenavItem>
+                <SidenavLink href="/#">
+                  <RodsIconMoney2 /> Werk &amp; Inkomen
+                </SidenavLink>
+              </SidenavItem>
+              <SidenavItem>
+                <SidenavLink href="/#">
+                  <RodsIconParking /> Parkeren
+                </SidenavLink>
+              </SidenavItem>
+            </SidenavList>
+            <SidenavList>
+              <SidenavItem>
+                <SidenavLink href="/#">
+                  <RodsIconUser />
+                  Mijn gegevens
+                </SidenavLink>
+              </SidenavItem>
+            </SidenavList>
+          </Sidenav>
         </div>
         <div className={'rods-grid__two-third'} style={{ backgroundColor: 'olive' }}>
-          <h2>Two third</h2>
-        </div>
-        <div className={'rods-grid__two-third'} style={{ backgroundColor: 'lightcoral' }}>
-          <h2>Two third</h2>
-        </div>
-        <div className={'rods-grid__one-third'} style={{ backgroundColor: 'blanchedalmond' }}>
-          <h2>One third</h2>
-        </div>
-        <div className={'rods-grid__half-width'} style={{ backgroundColor: 'cornflowerblue' }}>
-          <h2>Half width</h2>
-        </div>
-        <div className={'rods-grid__half-width'} style={{ backgroundColor: 'olive' }}>
-          <h2>Half width</h2>
+          <h1>Hallo mevrouw van Bergenhenegouwen</h1>
+          <p>
+            Via Mijn Loket kunt u veel zelf regelen met de gemeente. Bijvoorbeeld een afspraak maken om uw paspoort te
+            verlengen, of een subsidie aanvraag indienen. U kunt ook zien welke dingen u nog moet regelen of die wij van
+            u vragen. Bijvoorbeeld het betalen van gemeentelijke belastingen. Zie hiervoor ‘Wat ik moet regelen’.
+          </p>
         </div>
       </div>
     </div>
