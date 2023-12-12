93ae984b5b51e4174ffa1663bc7ed065140d6413
Bryan de Jong <bryandejong99@gmail.com>
2023-12-12 15:57:35 +0000

    docs: add grid to template story

--- packages/storybook/src/template/page-mijn-loket.stories.tsx
+++ packages/storybook/src/template/page-mijn-loket.stories.tsx
@@ -1,4 +1,7 @@
+/* @license CC0-1.0 */
+
 import { Meta, StoryObj } from '@storybook/react';
+import '@gemeente-rotterdam/components-css/grid/index.scss';
 
 const meta = {
   title: 'Template/Mijn Loket',
@@ -11,8 +14,31 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   render: (args) => (
-    <div {...args}>
-      <p>Hello world!</p>
+    <div {...args} style={{ containerType: 'inline-size' }}>
+      <div className={'rods-grid'}>
+        <div className={'rods-grid__full-width'} style={{ backgroundColor: 'cadetblue' }}>
+          <h1>rods-grid</h1>
+          <p>Full width segment.</p>
+        </div>
+        <div className={'rods-grid__one-third'} style={{ backgroundColor: 'lightsalmon' }}>
+          <h2>One third</h2>
+        </div>
+        <div className={'rods-grid__two-third'} style={{ backgroundColor: 'olive' }}>
+          <h2>Two third</h2>
+        </div>
+        <div className={'rods-grid__two-third'} style={{ backgroundColor: 'lightcoral' }}>
+          <h2>Two third</h2>
+        </div>
+        <div className={'rods-grid__one-third'} style={{ backgroundColor: 'blanchedalmond' }}>
+          <h2>One third</h2>
+        </div>
+        <div className={'rods-grid__half-width'} style={{ backgroundColor: 'cornflowerblue' }}>
+          <h2>Half width</h2>
+        </div>
+        <div className={'rods-grid__half-width'} style={{ backgroundColor: 'olive' }}>
+          <h2>Half width</h2>
+        </div>
+      </div>
     </div>
   ),
 };
