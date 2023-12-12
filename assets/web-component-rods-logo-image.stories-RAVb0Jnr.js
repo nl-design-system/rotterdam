f2a87f220043b0c974a42359d72d7bee08a9a7bb
bddjong <bryandejong99@gmail.com>
2023-12-12 17:47:56 +0000

    Deploying to gh-pages from @ nl-design-system/rotterdam@e2189d2ff143d43c7e6539193f3a1f324b5318d3 🚀

--- assets/web-component-rods-logo-image.stories-RAVb0Jnr.js
+++
@@ -1,4 +0,0 @@
-import{R as t}from"./components--tf66rxr.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";const s=`<!-- @license CC0-1.0 -->
-
-# Logo image
-`;/* @license CC0-1.0 */const p={title:"Web Component/Logo image",id:"web-component-rods-logo-image",component:t,argTypes:{},args:{},tags:["autodocs"],parameters:{docs:{description:{component:s}}}},o={};var e,a,r;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:"{}",...(r=(a=o.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};const d=["Default"];export{o as Default,d as __namedExportsOrder,p as default};
