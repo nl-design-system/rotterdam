2b9beff41699655297b96c559165f9dfa453368f
Robbert Broersma <git@robbertbroersma.nl>
2023-12-14 18:38:22 +0100

    docs: add table examples from "Belastingportaal"

--- packages/storybook/src/css-utrecht-table.stories.tsx
+++ packages/storybook/src/css-utrecht-table.stories.tsx
@@ -1,17 +1,23 @@
 /* @license CC0-1.0 */
 
+import { RodsIconEdit } from '@gemeente-rotterdam/web-components-react';
 import type { Meta, StoryObj } from '@storybook/react';
 import {
+  ButtonLink,
+  IBANData,
+  PreserveData,
+  StatusBadge,
   Table,
   TableBody,
+  TableCaption,
   TableCell,
   TableFooter,
   TableHeader,
   TableHeaderCell,
   TableRow,
 } from '@utrecht/component-library-react';
-import '@gemeente-rotterdam/components-css/table/index.scss';
 import readme from './table.md?raw';
+import '@gemeente-rotterdam/components-css/table/index.scss';
 
 const meta = {
   title: 'CSS Component/Table',
@@ -42,16 +48,16 @@ export const Default: Story = {
     children: [
       <TableHeader>
         <TableRow>
-          <TableHeaderCell className="rods-table-header-cell" scope="col">
+          <TableHeaderCell className="rods-table__header-cell" scope="col">
             Column A
           </TableHeaderCell>
-          <TableHeaderCell className="rods-table-header-cell" scope="col">
+          <TableHeaderCell className="rods-table__header-cell" scope="col">
             Column B
           </TableHeaderCell>
-          <TableHeaderCell className="rods-table-header-cell" scope="col">
+          <TableHeaderCell className="rods-table__header-cell" scope="col">
             Column C
           </TableHeaderCell>
-          <TableHeaderCell className="rods-table-header-cell" scope="col">
+          <TableHeaderCell className="rods-table__header-cell" scope="col">
             Column d
           </TableHeaderCell>
         </TableRow>
@@ -88,16 +94,16 @@ export const Hover: Story = {
     children: [
       <TableHeader>
         <TableRow>
-          <TableHeaderCell className="rods-table-header-cell" scope="col">
+          <TableHeaderCell className="rods-table__header-cell" scope="col">
             Column A
           </TableHeaderCell>
-          <TableHeaderCell className="rods-table-header-cell" scope="col">
+          <TableHeaderCell className="rods-table__header-cell" scope="col">
             Column B
           </TableHeaderCell>
-          <TableHeaderCell className="rods-table-header-cell" scope="col">
+          <TableHeaderCell className="rods-table__header-cell" scope="col">
             Column C
           </TableHeaderCell>
-          <TableHeaderCell className="rods-table-header-cell" scope="col">
+          <TableHeaderCell className="rods-table__header-cell" scope="col">
             Column d
           </TableHeaderCell>
         </TableRow>
@@ -124,6 +130,143 @@ export const Hover: Story = {
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/?node-id=507-19314',
+    },
+  },
+};
+
+export const EditLink: Story = {
+  args: {
+    className: 'utrecht-table--row-hover',
+    children: [
+      <TableCaption>Lopende automatische incasso’s</TableCaption>,
+      <TableHeader>
+        <TableRow>
+          <TableHeaderCell className="rods-table__header-cell" scope="col">
+            Gebruikt voor
+          </TableHeaderCell>
+          <TableHeaderCell className="rods-table__header-cell" scope="col">
+            IBAN rekeningnummer
+          </TableHeaderCell>
+          <TableHeaderCell className="rods-table__header-cell" scope="col">
+            Machtigingskenmerk
+          </TableHeaderCell>
+          <TableHeaderCell className="rods-table__header-cell" scope="col">
+            Actief sinds
+          </TableHeaderCell>
+          <TableHeaderCell className="rods-table__header-cell" scope="col"></TableHeaderCell>
+        </TableRow>
+      </TableHeader>,
+      <TableBody>
+        <TableRow>
+          <TableCell>Gemeentelijke heffingen</TableCell>
+          <TableCell>
+            <IBANData value="NL95RABO0370654363" />
+          </TableCell>
+          <TableCell>
+            <PreserveData>RDAM-122347-001-11</PreserveData>
+          </TableCell>
+          <TableCell>06-10-2023</TableCell>
+          <TableCell>
+            <ButtonLink appearance="subtle-button">
+              <RodsIconEdit />
+              Wijzigen
+            </ButtonLink>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Marktgelden</TableCell>
+          <TableCell>
+            <IBANData value="NL95RABO0370654363" />
+          </TableCell>
+          <TableCell>
+            <PreserveData>RDAM-982347-234-102</PreserveData>
+          </TableCell>
+          <TableCell>06-10-2023</TableCell>
+          <TableCell>
+            <ButtonLink appearance="subtle-button">
+              <RodsIconEdit />
+              Wijzigen
+            </ButtonLink>
+          </TableCell>
+        </TableRow>
+      </TableBody>,
+    ],
+  },
+  name: 'Rows with edit link',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/ByWn4DjjJ6y4Wl1FXugQ3H/Belastingportaal-RPA?type=design&node-id=1709-30506&mode=design&t=n0a2MlRfL9OdT1RA-4',
+    },
+  },
+};
+
+export const Badges: Story = {
+  args: {
+    children: [
+      <TableCaption>Opbouw van het bedrag</TableCaption>,
+      <TableHeader>
+        <TableRow>
+          <TableHeaderCell className="rods-table__header-cell" scope="col">
+            Datum
+          </TableHeaderCell>
+          <TableHeaderCell className="rods-table__header-cell" scope="col">
+            Omschrijving
+          </TableHeaderCell>
+          <TableHeaderCell numericColumn className="rods-table__header-cell" scope="col">
+            Bedrag
+          </TableHeaderCell>
+          <TableHeaderCell className="rods-table__header-cell" scope="col"></TableHeaderCell>
+        </TableRow>
+      </TableHeader>,
+      <TableBody>
+        <TableRow>
+          <TableCell>31 januari 2023</TableCell>
+          <TableCell>Vordering</TableCell>
+          <TableCell numericColumn>€ 1.200</TableCell>
+          <TableCell></TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>25 maart 2023</TableCell>
+          <TableCell>Ontvangst</TableCell>
+          <TableCell numericColumn>-€ 100</TableCell>
+          <TableCell>
+            <StatusBadge status="ok" className="utrecht-status-badge--rods-green-blue">
+              betaald
+            </StatusBadge>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>25 april 2023</TableCell>
+          <TableCell>Ontvangst</TableCell>
+          <TableCell numericColumn>-€ 100</TableCell>
+          <TableCell>
+            <StatusBadge status="ok" className="utrecht-status-badge--rods-green-blue">
+              betaald
+            </StatusBadge>
+          </TableCell>
+        </TableRow>
+      </TableBody>,
+      <TableFooter>
+        <TableRow>
+          <TableHeaderCell scope="row" colSpan={2}>
+            Dit moet u betalen:
+          </TableHeaderCell>
+          <TableCell numericColumn>€ 1.000</TableCell>
+          <TableCell>
+            <StatusBadge status="fixme" className="utrecht-status-badge--rods-light-blue">
+              nog te betalen
+            </StatusBadge>
+          </TableCell>
+        </TableRow>
+      </TableFooter>,
+    ],
+  },
+  name: 'Rows with badges',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/ByWn4DjjJ6y4Wl1FXugQ3H/Belastingportaal-RPA?type=design&node-id=2687-18454&mode=design&t=n0a2MlRfL9OdT1RA-4',
     },
   },
 };
