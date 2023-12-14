89355d35cea50a4bdb2eeb9ae417ea2b00b21dc0
Robbert <mail@robbertbroersma.nl>
2023-12-14 23:51:02 +0000

    Deploying to gh-pages from @ nl-design-system/rotterdam@ca82ca9703fceb8a03ca83eb5173ad7efa8df82b 🚀

---
+++ assets/css-utrecht-badge-list.stories-KHEz4XHd.js
@@ -0,0 +1,19 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{a as o,L as n,D as r}from"./index-icM4dY7T.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./objectDestructuringEmpty-GZ-hTwpl.js";import"./extends-dGVwEr9R.js";const d=`<!-- @license CC0-1.0 -->
+
+# Badge list
+`,u={title:"CSS Component/Badge list",id:"css-utrecht-badge-list",component:o,argTypes:{children:{name:"Content",type:{name:"string",required:!0},defaultValue:""}},args:{children:""},tags:["autodocs"],parameters:{docs:{description:{component:d}}}},a={name:"Default",args:{children:[e.jsx(n,{href:"#",children:e.jsx(r,{children:"duurzaamheid"})}),e.jsx(n,{href:"#",children:e.jsx(r,{children:"schoon, heel en veilig"})})]},parameters:{design:{type:"figma",url:"https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/?node-id=16340-13147"}}};var t,s,i;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  name: 'Default',
+  args: {
+    children: [<Link href="#">
+        <DataBadge>duurzaamheid</DataBadge>
+      </Link>, <Link href="#">
+        <DataBadge>schoon, heel en veilig</DataBadge>
+      </Link>]
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/?node-id=16340-13147'
+    }
+  }
+}`,...(i=(s=a.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};const f=["Default"];export{a as Default,f as __namedExportsOrder,u as default};
---
+++ assets/preview-mucyuQlj.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-dHONsXAe.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-XHMgL0tj.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-XHMgL0tj.js","./chunk-HLWAVYOI-2u82Pl0h.js","./iframe-dHONsXAe.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-SjJut8t4.js","./extends-dGVwEr9R.js","./index-mmcFPWCw.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
