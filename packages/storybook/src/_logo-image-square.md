fbe1e229e44d538c63a87d1702da4454a1b20b1f
Robbert Broersma <git@robbertbroersma.nl>
2023-12-14 20:17:18 +0100

    feat: use official logo SVGs #48

---
+++ packages/storybook/src/_logo-image-square.md
@@ -0,0 +1,1 @@
+If there isn’t enough space to use the base logo you’re allowed to use the ‘Square logo’.
--- packages/storybook/src/web-component-rods-logo-image.stories.tsx
+++ packages/storybook/src/web-component-rods-logo-image.stories.tsx
@@ -1,7 +1,16 @@
 /* @license CC0-1.0 */
 
-import { RodsLogoImage } from '@gemeente-rotterdam/web-components-react';
+import {
+  RodsLogoImage,
+  RodsLogoImageAvatar,
+  RodsLogoImageMark,
+  RodsLogoImageSquare,
+} from '@gemeente-rotterdam/web-components-react';
 import type { Meta, StoryObj } from '@storybook/react';
+import logoImageAvatarDocs from './_logo-image-avatar.md?raw';
+import logoImageBaseDocs from './_logo-image-base.md?raw';
+import logoImageMarkDocs from './_logo-image-mark.md?raw';
+import logoImageSquareDocs from './_logo-image-square.md?raw';
 import readme from './logo-image.md?raw';
 
 const meta = {
@@ -24,4 +33,49 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Base: Story = {
+  name: 'Base logo',
+  parameters: {
+    docs: {
+      description: {
+        story: logoImageBaseDocs,
+      },
+    },
+  },
+};
+
+export const Square: Story = {
+  name: 'Square logo',
+  parameters: {
+    docs: {
+      description: {
+        story: logoImageSquareDocs,
+      },
+    },
+  },
+  render: () => <RodsLogoImageSquare />,
+};
+
+export const Mark: Story = {
+  name: 'Mark',
+  parameters: {
+    docs: {
+      description: {
+        story: logoImageMarkDocs,
+      },
+    },
+  },
+  render: () => <RodsLogoImageMark />,
+};
+
+export const Avatar: Story = {
+  name: 'Avatars & favicons',
+  parameters: {
+    docs: {
+      description: {
+        story: logoImageAvatarDocs,
+      },
+    },
+  },
+  render: () => <RodsLogoImageAvatar />,
+};
