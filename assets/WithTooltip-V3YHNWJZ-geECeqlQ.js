cff4ceba40cbc777d2bc5ccd19b23eeb39e9241b
Robbert <mail@robbertbroersma.nl>
2023-12-14 16:44:54 +0000

    Deploying to gh-pages from @ nl-design-system/rotterdam@d7cee83cabab8ecca1df0693a9e6dbbe65994ca3 🚀

---
+++ assets/WithTooltip-V3YHNWJZ-geECeqlQ.js
@@ -0,0 +1,1 @@
+import{W as e,W as h,a as l}from"./index-rp9cNBL5.js";import"./iframe-tceg8Xfq.js";import"../sb-preview/runtime.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-jmm5gWkb.js";import"./extends-dGVwEr9R.js";import"./index-mmcFPWCw.js";export{e as WithToolTipState,h as WithTooltip,l as WithTooltipPure};
---
+++ assets/web-component-button.stories-i-QSQThq.js
@@ -0,0 +1,6 @@
+import{j as r}from"./jsx-runtime-vNq4Oc-g.js";import{E as s}from"./components-lf_m5hCU.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";const m=`<!-- @license CC0-1.0 -->
+
+# Example Button component
+`,p=({children:a})=>r.jsx(s,{children:a}),l={title:"Web Component/Example button (do not use)",id:"web-component-example-button",component:p,argTypes:{children:{name:"Content",description:"Button text",type:{name:"string",required:!0},defaultValue:""}},args:{children:"Opslaan en verder"},tags:["autodocs"],parameters:{docs:{description:{component:m}}}},e={name:"Example button"};var t,o,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  name: 'Example button'
+}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,l as default};
