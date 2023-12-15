87d921168caacd31b6cf3ffd7bd64885d5e090eb
Robbert <mail@robbertbroersma.nl>
2023-12-15 10:44:21 +0000

    Deploying to gh-pages from @ nl-design-system/rotterdam@7df62ba83d2c702a86f7d807ae9e187016ce3dab 🚀

---
+++ assets/badge-list-PRnreM6v.js
@@ -0,0 +1,4 @@
+const e=`<!-- @license CC0-1.0 -->
+
+# Badge list
+`;export{e as r};
---
+++ assets/css-utrecht-alert.stories-YgsDBNXO.js
@@ -0,0 +1,68 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{A as z,P as s}from"./index-saVp4LRQ.js";import{h as w,i as y,j as P,k}from"./components-15-7Mt0n.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./objectDestructuringEmpty-GZ-hTwpl.js";import"./extends-dGVwEr9R.js";const B=`<!-- @license CC0-1.0 -->
+
+# Alert
+`,W={title:"CSS Component/Alert",id:"css-utrecht-alert",component:z,argTypes:{children:{name:"Content",type:{name:"string",required:!0},defaultValue:""}},args:{children:""},tags:["autodocs"],parameters:{docs:{description:{component:B}}}},n={name:"Notification",args:{children:e.jsx(s,{children:"Deze gegevens zijn reeds ingevuld en komen uit Basis Registratie Personen (BRP). Indien deze gegevens niet kloppen, kunt u dit wijzigen in het BRP."}),icon:e.jsx(w,{}),type:"info"},parameters:{design:{type:"figma",url:"https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/?node-id=507-21455"}}},i={name:"Alert",args:{children:e.jsx(s,{children:"Deze gegevens zijn reeds ingevuld en komen uit Basis Registratie Personen (BRP). Indien deze gegevens niet kloppen, kunt u dit wijzigen in het BRP."}),icon:e.jsx(y,{}),type:"error"},parameters:{design:{type:"figma",url:"https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/Gemeente-Rotterdam-Design-System?type=design&node-id=507-21457"}}},r={name:"Warning",args:{children:e.jsx(s,{children:"Deze gegevens zijn reeds ingevuld en komen uit Basis Registratie Personen (BRP). Indien deze gegevens niet kloppen, kunt u dit wijzigen in het BRP."}),icon:e.jsx(P,{}),type:"warning"},parameters:{design:{type:"figma",url:"https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/Gemeente-Rotterdam-Design-System?type=design&node-id=507-21465&mode=design&t=yvzUSkFQYQmWSHsQ-4"}}},t={name:"OK",args:{children:e.jsx(s,{children:"Deze gegevens zijn reeds ingevuld en komen uit Basis Registratie Personen (BRP). Indien deze gegevens niet kloppen, kunt u dit wijzigen in het BRP."}),icon:e.jsx(k,{}),type:"ok"},parameters:{design:{type:"figma",url:"https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/Gemeente-Rotterdam-Design-System?type=design&node-id=507-21473&mode=design&t=yvzUSkFQYQmWSHsQ-4"}}};var a,o,g;n.parameters={...n.parameters,docs:{...(a=n.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  name: 'Notification',
+  args: {
+    children: <Paragraph>
+        Deze gegevens zijn reeds ingevuld en komen uit Basis Registratie Personen (BRP). Indien deze gegevens niet
+        kloppen, kunt u dit wijzigen in het BRP.
+      </Paragraph>,
+    icon: <RodsIconBadgeInfo />,
+    type: 'info'
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/?node-id=507-21455'
+    }
+  }
+}`,...(g=(o=n.parameters)==null?void 0:o.docs)==null?void 0:g.source}}};var d,m,p;i.parameters={...i.parameters,docs:{...(d=i.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  name: 'Alert',
+  args: {
+    children: <Paragraph>
+        Deze gegevens zijn reeds ingevuld en komen uit Basis Registratie Personen (BRP). Indien deze gegevens niet
+        kloppen, kunt u dit wijzigen in het BRP.
+      </Paragraph>,
+    icon: <RodsIconBadgeAlertSolid />,
+    type: 'error'
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/Gemeente-Rotterdam-Design-System?type=design&node-id=507-21457'
+    }
+  }
+}`,...(p=(m=i.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var c,l,u;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  name: 'Warning',
+  args: {
+    children: <Paragraph>
+        Deze gegevens zijn reeds ingevuld en komen uit Basis Registratie Personen (BRP). Indien deze gegevens niet
+        kloppen, kunt u dit wijzigen in het BRP.
+      </Paragraph>,
+    icon: <RodsIconBadgeAlert />,
+    type: 'warning'
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/Gemeente-Rotterdam-Design-System?type=design&node-id=507-21465&mode=design&t=yvzUSkFQYQmWSHsQ-4'
+    }
+  }
+}`,...(u=(l=r.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var R,h,f;t.parameters={...t.parameters,docs:{...(R=t.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  name: 'OK',
+  args: {
+    children: <Paragraph>
+        Deze gegevens zijn reeds ingevuld en komen uit Basis Registratie Personen (BRP). Indien deze gegevens niet
+        kloppen, kunt u dit wijzigen in het BRP.
+      </Paragraph>,
+    icon: <RodsIconBadgeCheck />,
+    type: 'ok'
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/Gemeente-Rotterdam-Design-System?type=design&node-id=507-21473&mode=design&t=yvzUSkFQYQmWSHsQ-4'
+    }
+  }
+}`,...(f=(h=t.parameters)==null?void 0:h.docs)==null?void 0:f.source}}};const X=["Info","Error","Warning","OK"];export{i as Error,n as Info,t as OK,r as Warning,X as __namedExportsOrder,W as default};
