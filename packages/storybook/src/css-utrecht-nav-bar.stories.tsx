4f54e8758e52c2ad933f830869941ba4cebce633
Bryan de Jong <bryandejong99@gmail.com>
2023-12-15 09:45:43 +0000

    docs: add nav bar story

---
+++ packages/storybook/src/css-utrecht-nav-bar.stories.tsx
@@ -0,0 +1,56 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { NavBar, NavList, NavListLink } from '@utrecht/component-library-react/dist/css-module';
+import readme from './badge-list.md?raw';
+
+const meta = {
+  title: 'CSS Component/Nav bar',
+  id: 'css-utrecht-nav-bar',
+  component: NavBar,
+  argTypes: {
+    children: {
+      name: 'Content',
+      type: {
+        name: 'string',
+        required: true,
+      },
+      defaultValue: '',
+    },
+  },
+  args: {
+    children: '',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof NavBar>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  name: 'Default',
+  args: {
+    children: [
+      <NavList>
+        <NavListLink href="#">Translate</NavListLink>
+        <NavListLink href="#">Unilever Nederland Holding B.V.</NavListLink>
+        <NavListLink href="#">Uitloggen</NavListLink>
+        <NavListLink href="#">Winkelmandje</NavListLink>
+      </NavList>,
+    ],
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/Gemeente-Rotterdam-Design-System?type=design&node-id=1730-7404',
+    },
+  },
+};
