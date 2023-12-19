7672f693de89a7c945cd5d556340f241ebb24e2c
Robbert <mail@robbertbroersma.nl>
2023-12-19 23:05:47 +0000

    Deploying to gh-pages from @ nl-design-system/rotterdam@e75933b2f06ed65b5bd782070131380db509f37d 🚀

---
+++ assets/css-utrecht-ordered-list.stories-20z6leH9.js
@@ -0,0 +1,15 @@
+import{j as n}from"./jsx-runtime-vNq4Oc-g.js";import{O as i,k as r}from"./index-2todkrdz.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./objectDestructuringEmpty-3xqcqj12.js";import"./typeof-Q9eVcF_1.js";import"./extends-dGVwEr9R.js";const d=`<!-- @license CC0-1.0 -->
+
+# Ordered list
+`,w={title:"CSS Component/Ordered list",id:"css-utrecht-ordered-list",component:i,argTypes:{children:{name:"Content",type:{name:"string",required:!0},defaultValue:""}},args:{children:""},tags:["autodocs"],parameters:{bugs:"https://github.com/nl-design-system/rotterdam/labels/component%2Fordered-list",design:{type:"figma",url:"https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/?node-id=490-17441"},docs:{description:{component:d}}}},e={name:"Ordered list",args:{children:[n.jsx(r,{children:"In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)"}),n.jsx(r,{children:"In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)"}),n.jsx(r,{children:"In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)"})]}};var t,a,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  name: 'Ordered list',
+  args: {
+    children: [<OrderedListItem>
+        In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)
+      </OrderedListItem>, <OrderedListItem>
+        In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)
+      </OrderedListItem>, <OrderedListItem>
+        In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)
+      </OrderedListItem>]
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const k=["Default"];export{e as Default,k as __namedExportsOrder,w as default};
--- assets/preview-VBfTnnPa.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-5hRII2ra.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-Ivlgcw9C.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-Ivlgcw9C.js","./chunk-HLWAVYOI-l47jowOr.js","./iframe-5hRII2ra.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-ba7OOUbL.js","./index-jmm5gWkb.js","./index-JsD6zNCV.js","./extends-dGVwEr9R.js","./index-mmcFPWCw.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
