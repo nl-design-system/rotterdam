1ce096bbcc297d23ce2b60e2b017c51c39f28fa6
Robbert <mail@robbertbroersma.nl>
2024-02-18 12:28:31 +0000

    Deploying to gh-pages from @ nl-design-system/rotterdam@797cb853b0ed67ab6669f19e2d16539766ca60fa 🚀

---
+++ assets/css-utrecht-button-group.stories-arM4Rm9I.js
@@ -0,0 +1,9 @@
+import{j as t}from"./jsx-runtime-vNq4Oc-g.js";import{g as s,h as n,i}from"./index-2todkrdz.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./objectDestructuringEmpty-3xqcqj12.js";import"./typeof-Q9eVcF_1.js";import"./extends-dGVwEr9R.js";const u=`<!-- @license CC0-1.0 -->
+
+# Button group
+`,h={title:"CSS Component/Button group",id:"css-utrecht-button-group",component:s,argTypes:{children:{name:"Content",type:{name:"string",required:!0},defaultValue:""}},args:{children:""},tags:["autodocs"],parameters:{bugs:"https://github.com/nl-design-system/rotterdam/labels/component%2Fbutton-group",design:{type:"figma",url:"https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/?node-id=771-28224"},docs:{description:{component:u}}}},e={name:"Default",args:{children:[t.jsx(n,{appearance:"primary-action-button",children:"Primary"}),t.jsx(n,{appearance:"secondary-action-button",children:"Label"}),t.jsx(n,{children:"Label"}),t.jsx(i,{children:"Text button"})]}};var o,r,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  name: 'Default',
+  args: {
+    children: [<Button appearance="primary-action-button">Primary</Button>, <Button appearance="secondary-action-button">Label</Button>, <Button>Label</Button>, <LinkButton>Text button</LinkButton>]
+  }
+}`,...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const B=["Default"];export{e as Default,B as __namedExportsOrder,h as default};
---
+++ assets/css-utrecht-paragraph.stories-k10aeM9W.js
@@ -0,0 +1,21 @@
+import{P as d}from"./index-2todkrdz.js";import"./objectDestructuringEmpty-3xqcqj12.js";import"./typeof-Q9eVcF_1.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./jsx-runtime-vNq4Oc-g.js";import"./extends-dGVwEr9R.js";const u=`<!-- @license CC0-1.0 -->
+
+# Paragraph
+`;/* @license CC0-1.0 */const P={title:"CSS Component/Paragraph",id:"css-utrecht-paragraph",component:d,argTypes:{children:{name:"Content",type:{name:"string",required:!0},defaultValue:""}},args:{children:""},tags:["autodocs"],parameters:{bugs:"https://github.com/nl-design-system/rotterdam/labels/component%2Fparagraph",design:{type:"figma",url:"https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/?node-id=415-12136"},docs:{description:{component:u}}}},e={name:"Paragraph",args:{children:"Een geveltuin aanleggen is leuk om te doen, de straat knapt ervan op en uw huis blijft in de zomer koeler. Meer groen maakt de stad ook beter bestand tegen klimaatveranderingen en wateroverlast en is goed voor insecten en vogels. Een geveltuin is een strook planten tegen de gevel van een woning. Ze zijn vooral geschikt in straten waar de huizen geen voortuin hebben. Eén enkele klimplant, stokroos of zonnebloem maakt het straatbeeld al gezelliger, maar andere planten kunnen natuurlijk ook. U kiest zelf."}},a={name:"Lead paragraph",args:{...e.args,lead:!0}},n={name:"Small print",args:{...e.args,small:!0}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: 'Paragraph',
+  args: {
+    children: 'Een geveltuin aanleggen is leuk om te doen, de straat knapt ervan op en uw huis blijft in de zomer koeler. Meer groen maakt de stad ook beter bestand tegen klimaatveranderingen en wateroverlast en is goed voor insecten en vogels. Een geveltuin is een strook planten tegen de gevel van een woning. Ze zijn vooral geschikt in straten waar de huizen geen voortuin hebben. Eén enkele klimplant, stokroos of zonnebloem maakt het straatbeeld al gezelliger, maar andere planten kunnen natuurlijk ook. U kiest zelf.'
+  }
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var s,l,i;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  name: 'Lead paragraph',
+  args: {
+    ...Default.args,
+    lead: true
+  }
+}`,...(i=(l=a.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};var g,m,p;n.parameters={...n.parameters,docs:{...(g=n.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  name: 'Small print',
+  args: {
+    ...Default.args,
+    small: true
+  }
+}`,...(p=(m=n.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const w=["Default","LeadParagraph","SmallPrintParagraph"];export{e as Default,a as LeadParagraph,n as SmallPrintParagraph,w as __namedExportsOrder,P as default};
