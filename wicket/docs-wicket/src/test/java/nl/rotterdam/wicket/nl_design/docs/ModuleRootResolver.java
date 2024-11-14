dba67df0dfad38ce0b4094d1b50c4d55b5ffd9d1
Kees van Dieren <keesvandieren@squins.com>
2024-11-14 11:12:09 +0100

    Make it run with Starter

---
+++ wicket/docs-wicket/src/test/java/nl/rotterdam/wicket/nl_design/docs/ModuleRootResolver.java
@@ -0,0 +1,22 @@
+package nl.rotterdam.wicket.nl_design.docs;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class ModuleRootResolver {
+
+    public static File resolveModuleRootPath(Class<?> classInProject) {
+        try {
+            URI uri = classInProject.getResource(".").toURI();
+            File file = new File(uri);
+            while (!new File(file, "pom.xml").exists()) {
+                file = file.getParentFile();
+            }
+
+            return file;
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
