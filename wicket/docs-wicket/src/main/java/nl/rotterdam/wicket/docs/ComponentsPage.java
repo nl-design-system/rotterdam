97ba9928ff9d9c8f7249e0f2cb49cdc108f14013
Kees van Dieren <keesvandieren@squins.com>
2024-11-20 08:43:31 +0100

    Add a link to make it stateful

--- wicket/docs-wicket/src/main/java/nl/rotterdam/wicket/docs/ComponentsPage.java
+++ wicket/docs-wicket/src/main/java/nl/rotterdam/wicket/docs/ComponentsPage.java
@@ -3,9 +3,14 @@ package nl.rotterdam.wicket.docs;
 import nl.rotterdam.nl_design.wicket.components.code_block.UtrechtCodeBlock;
 import nl.rotterdam.wicket.docs.heading.HeadingPanel;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ComponentsPage extends BasePage {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ComponentsPage.class);
 
     public ComponentsPage() {
 
@@ -20,5 +25,12 @@ public class ComponentsPage extends BasePage {
         add(new UtrechtCodeBlock("code-block-example", Model.of("Reload Page")));
 
         add(new HeadingPanel("headingComponents"));
+
+        add(new Link<Void>("refreshStatefulPageLink") {
+            @Override
+            public void onClick() {
+                LOGGER.info("Link was clicked. Page url now has state. Gives ability to inspect HTML in different state.");
+            }
+        });
     }
 }
