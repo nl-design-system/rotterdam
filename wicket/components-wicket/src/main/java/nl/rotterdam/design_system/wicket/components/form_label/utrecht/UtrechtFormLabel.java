9e5641c6beade614c68e5e347a83fa3f03423e11
Kees van Dieren <keesvandieren@squins.com>
2024-11-29 14:12:06 +0100

    No fields for components

--- wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/form_label/utrecht/UtrechtFormLabel.java
+++ wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/form_label/utrecht/UtrechtFormLabel.java
@@ -1,21 +1,19 @@
 package nl.rotterdam.design_system.wicket.components.form_label.utrecht;
 
-import static css.WicketComponentsCssReferences.FORM_LABEL_HEADER_ITEM;
-
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 
+import static css.WicketComponentsCssReferences.FORM_LABEL_HEADER_ITEM;
+
 public class UtrechtFormLabel extends Panel {
 
     public static final String SLOT_ID = "slot";
-    private final Label label;
 
     public UtrechtFormLabel(String id, IModel<?> textContent) {
         super(id);
-        label = new Label(SLOT_ID, textContent);
-        add(label);
+        add(new Label(SLOT_ID, textContent));
     }
 
     @Override
