1b153643f2b2d6f68d350675c4cbefc6a87384a4
Kees van Dieren <keesvandieren@squins.com>
2024-11-29 14:05:21 +0100

    Utrecht alert without :container but with div for maximum flexibility

--- wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/alert/utrecht/UtrechtAlert.java
+++ wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/alert/utrecht/UtrechtAlert.java
@@ -1,7 +1,5 @@
 package nl.rotterdam.design_system.wicket.components.alert.utrecht;
 
-import org.apache.wicket.markup.ComponentTag;
-import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
@@ -12,18 +10,7 @@ public class UtrechtAlert extends Panel {
 
     public UtrechtAlert(String id, IModel<String> textContent, UtrechtAlertType type) {
         super(id);
-        WebMarkupContainer container = new WebMarkupContainer("alert") {
-            @Override
-            public void onComponentTag(ComponentTag tag) {
-                super.onComponentTag(tag);
-
-                if (!tag.isClose()) {
-                    tag.append("class", "utrecht-alert--" + type.type, " ");
-                }
-            }
-        };
-        add(container);
-        container.add(UtrechtAlertBehavior.utrechtAlert(type));
-        container.add(new Label(SLOT_ID, textContent));
+        add(UtrechtAlertBehavior.utrechtAlert(type));
+        add(new Label(SLOT_ID, textContent));
     }
 }
