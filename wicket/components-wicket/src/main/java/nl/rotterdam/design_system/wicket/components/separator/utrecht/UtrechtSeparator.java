62f6faa2bdf4e020e9fa49cfb215ce06fdeee0c7
Robbert Broersma <git@robbertbroersma.nl>
2024-11-29 12:41:23 +0100

    refactor: convert Wicket Separator from Panel to WebComponent

--- wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/separator/utrecht/UtrechtSeparator.java
+++ wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/separator/utrecht/UtrechtSeparator.java
@@ -1,10 +1,27 @@
 package nl.rotterdam.design_system.wicket.components.separator.utrecht;
 
-import org.apache.wicket.markup.html.panel.Panel;
+import static css.WicketComponentsCssReferences.SEPARATOR_HEADER_ITEM;
 
-public class UtrechtSeparator extends Panel {
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.html.WebComponent;
+
+public class UtrechtSeparator extends WebComponent {
 
     public UtrechtSeparator(String id) {
         super(id);
+    }
+
+    @Override
+    protected void onComponentTag(ComponentTag tag) {
+        super.onComponentTag(tag);
+        tag.setName("hr");
+        tag.put("class", "utrecht-separator");
+    }
+
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        super.renderHead(response);
+        response.render(SEPARATOR_HEADER_ITEM);
     }
 }
