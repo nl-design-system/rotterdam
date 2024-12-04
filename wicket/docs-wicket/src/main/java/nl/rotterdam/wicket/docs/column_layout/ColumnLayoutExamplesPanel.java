ed45bc0cc8ffd5daf3cbb977c754ed5ae628deb4
Robbert Broersma <git@robbertbroersma.nl>
2024-12-04 01:21:10 +0100

    test: add Column Layout stories

---
+++ wicket/docs-wicket/src/main/java/nl/rotterdam/wicket/docs/column_layout/ColumnLayoutExamplesPanel.java
@@ -0,0 +1,25 @@
+package nl.rotterdam.wicket.docs.column_layout;
+
+import nl.rotterdam.design_system.wicket.components.column_layout.utrecht.UtrechtColumnLayoutBorder;
+import nl.rotterdam.wicket.docs.ComponentExample;
+import org.apache.wicket.Component;
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class ColumnLayoutExamplesPanel extends Panel {
+
+    public ColumnLayoutExamplesPanel(String id) {
+        super(id);
+    }
+
+    @ComponentExample
+    private static Component exampleColumnLayout() {
+        return new UtrechtColumnLayoutBorder("utrechtColumnLayout");
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+
+        add(exampleColumnLayout());
+    }
+}
