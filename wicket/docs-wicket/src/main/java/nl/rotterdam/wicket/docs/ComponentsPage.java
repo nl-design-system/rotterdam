e11a953bf6e7838dff5f2090b1464d6ff536586a
Robbert Broersma <git@robbertbroersma.nl>
2024-12-06 09:04:59 +0100

    docs: add component examples to dev server

--- wicket/docs-wicket/src/main/java/nl/rotterdam/wicket/docs/ComponentsPage.java
+++ wicket/docs-wicket/src/main/java/nl/rotterdam/wicket/docs/ComponentsPage.java
@@ -1,8 +1,10 @@
 package nl.rotterdam.wicket.docs;
 
 import css.DesignSystemTheme;
-import nl.rotterdam.design_system.wicket.components.alert.utrecht.UtrechtAlert;
-import nl.rotterdam.design_system.wicket.components.alert.utrecht.UtrechtAlertType;
+import java.util.Arrays;
+import java.util.List;
+// import nl.rotterdam.design_system.wicket.components.alert.utrecht.UtrechtAlert;
+// import nl.rotterdam.design_system.wicket.components.alert.utrecht.UtrechtAlertType;
 import nl.rotterdam.design_system.wicket.components.button_group.utrecht.UtrechtButtonGroupBorder;
 import nl.rotterdam.design_system.wicket.components.code_block.utrecht.UtrechtCodeBlock;
 import nl.rotterdam.design_system.wicket.components.column_layout.utrecht.UtrechtColumnLayoutBorder;
@@ -22,13 +24,36 @@ import nl.rotterdam.design_system.wicket.components.page_header.utrecht.UtrechtP
 import nl.rotterdam.design_system.wicket.components.page_layout.utrecht.UtrechtPageLayoutBorder;
 import nl.rotterdam.design_system.wicket.components.separator.utrecht.UtrechtSeparator;
 import nl.rotterdam.design_system.wicket.components.unordered_list.utrecht.UtrechtUnorderedList;
-import nl.rotterdam.wicket.docs.form_label.FormLabelExamplesPanel;
+import nl.rotterdam.wicket.docs.alert.AlertExamplesPanel;
+import nl.rotterdam.wicket.docs.badge_list.BadgeListExamplesPanel;
+import nl.rotterdam.wicket.docs.body.BodyExamplesPanel;
+import nl.rotterdam.wicket.docs.button_group.ButtonGroupExamplesPanel;
+import nl.rotterdam.wicket.docs.code_block.CodeBlockExamplesPanel;
+import nl.rotterdam.wicket.docs.column_layout.ColumnLayoutExamplesPanel;
+import nl.rotterdam.wicket.docs.form_field_checkbox.FormFieldCheckboxExamplesPanel;
+import nl.rotterdam.wicket.docs.form_field_label.FormFieldLabelExamplesPanel;
+// import nl.rotterdam.wicket.docs.form_label.FormLabelExamplesPanel;
 import nl.rotterdam.wicket.docs.heading.HeadingExamplesPanel;
+import nl.rotterdam.wicket.docs.heading_group.HeadingGroupExamplesPanel;
+import nl.rotterdam.wicket.docs.html_content.HtmlContentExamplesPanel;
+import nl.rotterdam.wicket.docs.icon.IconExamplesPanel;
+import nl.rotterdam.wicket.docs.logo.UtrechtLogoExamplesPanel;
+import nl.rotterdam.wicket.docs.logo_image.RotterdamLogoImageExamplesPanel;
+import nl.rotterdam.wicket.docs.number_badge.UtrechtNumberBadgeExamplesPanel;
+import nl.rotterdam.wicket.docs.ordered_list.UtrechtOrderedListExamplesPanel;
+import nl.rotterdam.wicket.docs.page_body.UtrechtPageBodyExamplesPanel;
+import nl.rotterdam.wicket.docs.page_footer.UtrechtPageFooterExamplesPanel;
+import nl.rotterdam.wicket.docs.page_layout.UtrechtPageLayoutExamplesPanel;
+import nl.rotterdam.wicket.docs.preserve_data.UtrechtPreserveDataExamplesPanel;
+import nl.rotterdam.wicket.docs.root.UtrechtRootExamplesPanel;
+import nl.rotterdam.wicket.docs.separator.UtrechtSeparatorExamplesPanel;
 import nl.rotterdam.wicket.docs.unordered_list.Kind;
+import nl.rotterdam.wicket.docs.unordered_list.UtrechtUnorderedListExamplesPanel;
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.head.JavaScriptReferenceHeaderItem;
+import org.apache.wicket.markup.head.JavaScriptReferenceType;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.DropDownChoice;
@@ -41,9 +66,6 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class ComponentsPage extends BasePage {
 
@@ -83,6 +105,8 @@ public class ComponentsPage extends BasePage {
     @Override
     protected void onInitialize() {
         super.onInitialize();
+
+        // Build up the page layout
         pageLayout = new UtrechtPageLayoutBorder("utrechtPageLayout");
         add(pageLayout);
 
@@ -102,11 +126,37 @@ public class ComponentsPage extends BasePage {
 
         pageBody.add(new UtrechtCodeBlock("code-block-example", Model.of("Reload Page")));
 
+        // Add the component stories
+        pageBody.add(new AlertExamplesPanel("alertExamplesPanel"));
+        pageBody.add(new BadgeListExamplesPanel("badgeListExamplesPanel"));
+        pageBody.add(new BodyExamplesPanel("bodyExamplesPanel"));
+        pageBody.add(new ButtonGroupExamplesPanel("buttonGroupExamplesPanel"));
+        pageBody.add(new CodeBlockExamplesPanel("codeBlockExamplesPanel"));
+        pageBody.add(new ColumnLayoutExamplesPanel("columnLayoutExamplesPanel"));
+        pageBody.add(new FormFieldCheckboxExamplesPanel("formFieldCheckboxExamplesPanel"));
+        pageBody.add(new FormFieldLabelExamplesPanel("formFieldLabelExamplesPanel"));
+        pageBody.add(new HeadingExamplesPanel("headingExamplesPanel"));
+        pageBody.add(new HeadingGroupExamplesPanel("headingGroupExamplesPanel"));
+        pageBody.add(new HtmlContentExamplesPanel("htmlContentExamplesPanel"));
+        pageBody.add(new IconExamplesPanel("iconExamplesPanel"));
+        pageBody.add(new RotterdamLogoImageExamplesPanel("rotterdamLogoImageExamplesPanel"));
+        pageBody.add(new UtrechtLogoExamplesPanel("logoExamplesPanel"));
+        pageBody.add(new UtrechtNumberBadgeExamplesPanel("numberBadgeExamplesPanel"));
+        pageBody.add(new UtrechtOrderedListExamplesPanel("orderedListExamplesPanel"));
+        pageBody.add(new UtrechtPageBodyExamplesPanel("pageBodyExamplesPanel"));
+        pageBody.add(new UtrechtPageFooterExamplesPanel("pageFooterExamplesPanel"));
+        pageBody.add(new UtrechtPageLayoutExamplesPanel("pageLayoutExamplesPanel"));
+        pageBody.add(new UtrechtPreserveDataExamplesPanel("preserveDataExamplesPanel"));
+        pageBody.add(new UtrechtRootExamplesPanel("rootExamplesPanel"));
+        pageBody.add(new UtrechtSeparatorExamplesPanel("separatorExamplesPanel"));
+        pageBody.add(new UtrechtUnorderedListExamplesPanel("unorderedListExamplesPanel"));
+
         pageBody.add(new HeadingExamplesPanel("headingComponents"));
         // add(new HeadingPanel("labelComponents"));
-        pageBody.add(new FormLabelExamplesPanel("labelComponents"));
+        // pageBody.add(new FormFieldLabelExamplesPanel("labelComponents"));
         pageBody.add(new UtrechtHtmlContentBorder("utrechtHtmlContent"));
         pageBody.add(new UtrechtNumberBadge("utrechtNumberBadge", Model.of("42")));
+        /*
         pageBody.add(
             new UtrechtAlert(
                 "utrechtAlertInfo",
@@ -143,6 +193,7 @@ public class ComponentsPage extends BasePage {
                 UtrechtAlertType.OK
             )
         );
+         */
         pageBody.add(new UtrechtSeparator("utrechtSeparator"));
         pageBody.add(new UtrechtButtonGroupBorder("utrechtButtonGroup"));
         orderedList = new UtrechtOrderedList("utrechtOrderedList");
@@ -173,36 +224,38 @@ public class ComponentsPage extends BasePage {
         //     }
         // );
 
-
         List<Kind> kinderen = List.of(
             new Kind("Jan", 2000, 123456789),
             new Kind("Storm", 2001, 123456790),
             new Kind("Fatima", 2002, 123456791)
         );
 
+        pageBody.add(
+            new WebMarkupContainer("utrechtOrderedListWebMarkupContainer") {
+                @Override
+                protected void onInitialize() {
+                    super.onInitialize();
 
-        pageBody.add(new WebMarkupContainer("utrechtOrderedListWebMarkupContainer") {
-            @Override
-            protected void onInitialize() {
-                super.onInitialize();
-
-                add(new UtrechtOrderedListBehavior()); // TODO should be singleton
-                add(new ListView<Kind>("listItem", kinderen) {
-
-                        @Override
-                        protected void populateItem(ListItem<Kind> item) {
-                            Kind kind = item.getModelObject();
-                            item.add(new UtrechtOrderedListItemBehavior()); // TODO: should be singleton
-                            item.add(new ExternalLink("link",
-                                "https://concern.ir.rotterdam.nl/example/kind/" + kind.administratienummer(),
-                                kind.naam() + " - " + kind.geboortejaar())
-                            );
-
+                    add(new UtrechtOrderedListBehavior()); // TODO should be singleton
+                    add(
+                        new ListView<Kind>("listItem", kinderen) {
+                            @Override
+                            protected void populateItem(ListItem<Kind> item) {
+                                Kind kind = item.getModelObject();
+                                item.add(new UtrechtOrderedListItemBehavior()); // TODO: should be singleton
+                                item.add(
+                                    new ExternalLink(
+                                        "link",
+                                        "https://concern.ir.rotterdam.nl/example/kind/" + kind.administratienummer(),
+                                        kind.naam() + " - " + kind.geboortejaar()
+                                    )
+                                );
+                            }
                         }
-                    }
-                );
+                    );
+                }
             }
-        });
+        );
 
         // RepeatingView listItems = new RepeatingView("utrechtOrderedListItem");
         // listItems.add(new Label(listItems.newChildId(), "green"));
@@ -227,6 +280,8 @@ public class ComponentsPage extends BasePage {
     @Override
     public void renderHead(IHeaderResponse response) {
         super.renderHead(response);
+
+        COMPONENTS_JS_HEADER_ITEM.setType(JavaScriptReferenceType.MODULE);
         response.render(COMPONENTS_JS_HEADER_ITEM);
     }
 
