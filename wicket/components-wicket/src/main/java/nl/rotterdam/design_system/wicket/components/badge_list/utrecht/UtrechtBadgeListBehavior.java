130ae3dc4c2766af8ff3936c73790a4dfdbb39fc
Kees van Dieren <keesvandieren@squins.com>
2024-12-06 15:57:43 +0100

    Centralize logic for creating css header items

--- wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/badge_list/utrecht/UtrechtBadgeListBehavior.java
+++ wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/badge_list/utrecht/UtrechtBadgeListBehavior.java
@@ -2,16 +2,14 @@ package nl.rotterdam.design_system.wicket.components.badge_list.utrecht;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
-import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.CssReferenceHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.request.resource.CssResourceReference;
+
+import static nl.rotterdam.design_system.wicket.components.CssReferenceHeaderItems.cssReferenceHeaderItem;
 
 public class UtrechtBadgeListBehavior extends Behavior {
 
-    public static final CssReferenceHeaderItem CSS = CssHeaderItem.forReference(
-        new CssResourceReference(UtrechtBadgeListBehavior.class, "@utrecht/badge-list-css/dist/index.css")
-    );
+    public static final CssReferenceHeaderItem CSS = cssReferenceHeaderItem(UtrechtBadgeListBehavior.class, "@utrecht/badge-list-css/dist/index.css");
 
     @Override
     public void renderHead(Component component, IHeaderResponse response) {
--- wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/page_header/utrecht/UtrechtPageHeaderBehavior.java
+++ wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/page_header/utrecht/UtrechtPageHeaderBehavior.java
@@ -2,16 +2,15 @@ package nl.rotterdam.design_system.wicket.components.page_header.utrecht;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
-import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.CssReferenceHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.request.resource.CssResourceReference;
+
+import static nl.rotterdam.design_system.wicket.components.CssReferenceHeaderItems.cssReferenceHeaderItem;
 
 public class UtrechtPageHeaderBehavior extends Behavior {
 
-    public static final CssReferenceHeaderItem CSS = CssHeaderItem.forReference(
-        new CssResourceReference(UtrechtPageHeaderBehavior.class, "@utrecht/page-header-css/dist/index.css")
-    );
+    public static final CssReferenceHeaderItem CSS =
+        cssReferenceHeaderItem(UtrechtPageHeaderBehavior.class, "@utrecht/page-header-css/dist/index.css");
 
     @Override
     public void renderHead(Component component, IHeaderResponse response) {
