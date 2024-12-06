df87f975f1945c1e19a077ea98b7d39f1708ebd7
Robbert Broersma <git@robbertbroersma.nl>
2024-12-06 02:13:50 +0100

    test: add Unordered List stories

--- wicket/docs-wicket/src/main/java/nl/rotterdam/wicket/docs/unordered_list/Kind.java
+++
@@ -1,6 +0,0 @@
-package nl.rotterdam.wicket.docs.unordered_list;
-
-import java.io.Serializable;
-
-public record Kind(String naam, int geboortejaar, long administratienummer) implements Serializable {
-}
