3b5e0e798d06187efc11cf1d28a67a44e6af1e16
Kees van Dieren <keesvandieren@squins.com>
2024-12-06 17:03:54 +0100

    Add ability to include rendered markup instead of source markup

--- wicket/docs-wicket/src/test/java/nl/rotterdam/wicket/nl_design/docs/storybook_generator/HtmlDocumentationExtractor.java
+++ wicket/docs-wicket/src/test/java/nl/rotterdam/wicket/nl_design/docs/storybook_generator/HtmlDocumentationExtractor.java
@@ -1,14 +1,15 @@
 package nl.rotterdam.wicket.nl_design.docs.storybook_generator;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import com.google.common.base.Charsets;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.Optional;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class HtmlDocumentationExtractor {
 
@@ -20,6 +21,10 @@ public class HtmlDocumentationExtractor {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public HtmlDocumentationExtractor(String renderedHtml) {
+        document = Jsoup.parse(renderedHtml);
     }
 
     public WicketHtmlExampleSnippet extractExample(String wicketId) {
