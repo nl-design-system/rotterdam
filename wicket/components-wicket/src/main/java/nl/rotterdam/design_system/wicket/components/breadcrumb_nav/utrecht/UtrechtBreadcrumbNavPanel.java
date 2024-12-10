1f02e3cecf1c46668754a38da8bd158206ebf98c
Robbert Broersma <git@robbertbroersma.nl>
2024-12-10 19:33:18 +0100

    feat: add Breadcrumb Navigation component

---
+++ wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/breadcrumb_nav/utrecht/UtrechtBreadcrumbNavPanel.java
@@ -0,0 +1,86 @@
+package nl.rotterdam.design_system.wicket.components.breadcrumb_nav.utrecht;
+
+import java.util.List;
+import nl.rotterdam.design_system.wicket.components.icon.rotterdam.RotterdamIconBehavior;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.request.component.IRequestablePage;
+
+public class UtrechtBreadcrumbNavPanel extends Panel {
+
+    public List<UtrechtBreadcrumbNavRecord<? extends IRequestablePage>> links;
+
+    public UtrechtBreadcrumbNavPanel(String id, List<UtrechtBreadcrumbNavRecord<? extends IRequestablePage>> links) {
+        super(id);
+        this.links = links;
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        List<UtrechtBreadcrumbNavRecord<? extends IRequestablePage>> links = this.links;
+
+        add(
+            new WebMarkupContainer("utrechtBreadcrumbNav") {
+                @Override
+                protected void onInitialize() {
+                    super.onInitialize();
+                    add(new UtrechtBreadcrumbNavBehavior());
+
+                    add(
+                        new WebMarkupContainer("utrechtBreadcrumbNavList") {
+                            @Override
+                            protected void onInitialize() {
+                                super.onInitialize();
+
+                                add(new UtrechtBreadcrumbNavListBehavior()); // TODO should be singleton
+                                add(
+                                    new ListView<UtrechtBreadcrumbNavRecord<? extends IRequestablePage>>(
+                                        "utrechtBreadcrumbNavItem",
+                                        links
+                                    ) {
+                                        @Override
+                                        protected void populateItem(
+                                            ListItem<UtrechtBreadcrumbNavRecord<? extends IRequestablePage>> item
+                                        ) {
+                                            item.add(new UtrechtBreadcrumbNavItemBehavior()); // TODO: should be singleton
+                                            UtrechtBreadcrumbNavRecord<? extends IRequestablePage> record =
+                                                item.getModelObject();
+                                            String naam = record.label();
+
+                                            // TODO: How do I type this?
+                                            @SuppressWarnings({ "rawtypes", "unchecked" })
+                                            UtrechtBreadcrumbNavLink<?> link = new UtrechtBreadcrumbNavLink(
+                                                "utrechtBreadcrumbNavLink",
+                                                record.target()
+                                            );
+
+                                            // Optionally add an icon
+                                            if (record.icon() != null) {
+                                                WebMarkupContainer icon = new WebMarkupContainer(
+                                                    "utrechtBreadcrumbNavLinkIcon"
+                                                );
+                                                icon.add(new RotterdamIconBehavior(record.icon()));
+                                                link.add(icon);
+                                            } else {
+                                                link.add(new Label("utrechtBreadcrumbNavLinkIcon", ""));
+                                            }
+
+                                            // Add the link text
+                                            link.add(new Label("utrechtBreadcrumbNavLinkLabel", naam));
+
+                                            item.add(link);
+                                        }
+                                    }
+                                );
+                            }
+                        }
+                    );
+                }
+            }
+        );
+    }
+}
