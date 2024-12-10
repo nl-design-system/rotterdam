98e75897ad71cc2e9e9d16cbf10ddc070d1fc7dc
Robbert Broersma <git@robbertbroersma.nl>
2024-12-10 19:33:18 +0100

    feat: add Breadcrumb Navigation component

---
+++ wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/breadcrumb_nav/utrecht/UtrechtBreadcrumbNavRecord.java
@@ -0,0 +1,7 @@
+package nl.rotterdam.design_system.wicket.components.breadcrumb_nav.utrecht;
+
+import java.io.Serializable;
+import nl.rotterdam.design_system.wicket.components.icon.rotterdam.RotterdamIconType;
+
+public record UtrechtBreadcrumbNavRecord<C>(RotterdamIconType icon, String label, Class<C> target)
+    implements Serializable {}
