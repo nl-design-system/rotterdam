8d161a4488b89f96933f4174506a9c2aabb3983a
Kees van Dieren <keesvandieren@squins.com>
2024-12-18 15:53:41 +0100

    Singletons for behaviors

--- wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/link_list/utrecht/UtrechtLinkListBehavior.java
+++ wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/link_list/utrecht/UtrechtLinkListBehavior.java
@@ -1,18 +1,20 @@
 package nl.rotterdam.design_system.wicket.components.link_list.utrecht;
 
-import static nl.rotterdam.design_system.wicket.components.CssReferenceHeaderItems.cssReferenceHeaderItem;
-
 import nl.rotterdam.design_system.wicket.components.base.TagNameClassComponentBehavior;
 import org.apache.wicket.markup.head.CssReferenceHeaderItem;
 
+import static nl.rotterdam.design_system.wicket.components.CssReferenceHeaderItems.cssReferenceHeaderItem;
+
 public class UtrechtLinkListBehavior extends TagNameClassComponentBehavior {
+
+    public static final UtrechtLinkListBehavior UTRECHT_LINK_LIST_BEHAVIOR = new UtrechtLinkListBehavior();
 
     public static final CssReferenceHeaderItem CSS = cssReferenceHeaderItem(
         UtrechtLinkListBehavior.class,
         "@utrecht/link-list-css/dist/index.css"
     );
 
-    public UtrechtLinkListBehavior() {
+    private UtrechtLinkListBehavior() {
         super("ul", "utrecht-link-list");
         addHeaderItem(CSS);
     }
