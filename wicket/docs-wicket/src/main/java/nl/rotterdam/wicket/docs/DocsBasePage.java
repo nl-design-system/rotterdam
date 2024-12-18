a2437be1be78f602441c92328d5bc36a91fefb8b
Kees van Dieren <keesvandieren@squins.com>
2024-12-18 15:33:41 +0100

    Match the page name the intent
    Other project in Rotterdam also has 'BasePage' class, making it a bit confusing

--- wicket/docs-wicket/src/main/java/nl/rotterdam/wicket/docs/DocsBasePage.java
+++ wicket/docs-wicket/src/main/java/nl/rotterdam/wicket/docs/DocsBasePage.java
@@ -1,22 +1,21 @@
 package nl.rotterdam.wicket.docs;
 
-import static css.DesignTokensWicketCssReference.THEME_ROTTERDAM_RODS_CSS_HEADER_ITEM;
-import static css.WicketComponentsCssReferences.THEME_VOORBEELD_GEMEENTE_CSS_HEADER_ITEM;
-
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.CssReferenceHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.resource.CssResourceReference;
 
-public abstract class BasePage extends WebPage {
+import static css.DesignTokensWicketCssReference.THEME_ROTTERDAM_RODS_CSS_HEADER_ITEM;
+import static css.WicketComponentsCssReferences.THEME_VOORBEELD_GEMEENTE_CSS_HEADER_ITEM;
 
-    public static final String TITLE_ID = "title";
+public abstract class DocsBasePage extends WebPage {
+
     public static final CssReferenceHeaderItem DOCS_CSS_HEADER_ITEM = CssHeaderItem.forReference(
-        new CssResourceReference(BasePage.class, "docs.css")
+        new CssResourceReference(DocsBasePage.class, "docs.css")
     );
 
-    public BasePage() {
+    public DocsBasePage() {
         super();
     }
 
