a94a7cf48bc3458091a47959657b3d9d3aca692b
Kees van Dieren <keesvandieren@squins.com>
2024-12-18 16:27:20 +0100

    Add checkbox css to java world

--- wicket/docs-wicket/src/main/java/nl/rotterdam/wicket/docs/sso_login_page/SingleSignOnLoginFormPanel.java
+++ wicket/docs-wicket/src/main/java/nl/rotterdam/wicket/docs/sso_login_page/SingleSignOnLoginFormPanel.java
@@ -3,7 +3,10 @@ package nl.rotterdam.wicket.docs.sso_login_page;
 import nl.rotterdam.design_system.wicket.components.button.utrecht.UtrechtButton;
 import nl.rotterdam.design_system.wicket.components.button_group.utrecht.UtrechtButtonGroupBorder;
 import nl.rotterdam.design_system.wicket.components.form_field_checkbox.utrecht.UtrechtFormFieldCheckbox;
-import org.apache.wicket.markup.html.form.*;
+import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
@@ -43,13 +46,19 @@ public class SingleSignOnLoginFormPanel extends Panel {
                 }
 
                 @Override
+                protected void onError() {
+                    super.onError();
+                    error("On error called. Only possible in classic browsers who don't support 'required' attribute.");
+                }
+
+                @Override
                 protected void onSubmit() {
+                    super.onSubmit();
+
                     info("Submitted!");
 
                     System.out.println("Received username: " + username.getObject());
                     System.out.println("Received rememberMe: " + rememberMe.getObject());
-
-                    super.onSubmit();
                 }
             }
         );
