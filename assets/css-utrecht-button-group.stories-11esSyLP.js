cd785b6f8d825d0a5b0b562e2629831abed46cc5
Yolijn <mail@yolijn.com>
2024-12-30 13:42:14 +0000

    Deploying to gh-pages from @ nl-design-system/rotterdam@61dd08db9d0d64fb0039c25817ed6df8fcf58a80 🚀

---
+++ assets/css-utrecht-button-group.stories-11esSyLP.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-BlAj40OV.js";import{h as s,c as n,i}from"./index-C0aWehL-.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./objectDestructuringEmpty-DWWjC7SK.js";import"./extends-CF3RwP-h.js";const u=`<!-- @license CC0-1.0 -->
+
+# Button group
+`,b={id:"css-utrecht-button-group",args:{children:""},argTypes:{children:{name:"Content",defaultValue:"",type:{name:"string",required:!0}}},component:s,parameters:{bugs:"https://github.com/nl-design-system/rotterdam/labels/component%2Fbutton-group",design:{type:"figma",url:"https://www.figma.com/design/iWuQsxelJzXVfCge87Og81/RODS-component?node-id=1-56&t=EXoPmctpkctdqa4z-4"},docs:{description:{component:u}}},tags:["autodocs"],title:"CSS Component/Button group"},e={name:"Default",args:{children:t.jsxs(t.Fragment,{children:[t.jsx(n,{appearance:"primary-action-button",children:"Primary"}),t.jsx(n,{appearance:"secondary-action-button",children:"Label"}),t.jsx(n,{children:"Label"}),t.jsx(i,{children:"Text button"})]})}};var o,r,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  name: 'Default',
+  args: {
+    children: <>
+        <Button appearance="primary-action-button">Primary</Button>
+        <Button appearance="secondary-action-button">Label</Button>
+        <Button>Label</Button>
+        <LinkButton>Text button</LinkButton>
+      </>
+  }
+}`,...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const h=["Default"];export{e as Default,h as __namedExportsOrder,b as default};
---
+++ assets/wicket-aan-de-slag-BttqMjWI.js
@@ -0,0 +1,9 @@
+import{j as t}from"./jsx-runtime-BlAj40OV.js";import{useMDXComponents as a}from"./index-BI1Biiay.js";import{aj as r,ak as s}from"./index-CmI1lUdS.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-B4dS-EDr.js";import"../sb-preview/runtime.js";import"./index-BU4L-DQy.js";import"./extends-CF3RwP-h.js";import"./index-DSgSp0X8.js";const i=`# Aan de slag als developer
+
+Je kunt op dit moment de laatste \`.jar\` vinden als download bij [GitHub Actions voor Apache Wicket bij de \`main\` branch](https://github.com/nl-design-system/example/blob/main/.github/workflows/continuous-deployment.yml#L166-L179).
+
+Klik de laatste "workflow run" aan en klik op de download link voor "Component library JAR file for Java 21".
+
+In de toekomst gaan we [automatisch releases doen met versienummers](https://github.com/nl-design-system/rotterdam/issues/170).
+`;function o(e){return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Apache Wicket/Aan de slag"}),`
+`,t.jsx(s,{children:i})]})}function f(e={}){const{wrapper:n}={...a(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o()}export{f as default};
