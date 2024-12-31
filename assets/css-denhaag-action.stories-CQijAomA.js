e60920e7d6a6243d9de5fe68fc1c85abc0f3305b
Robbert <mail@robbertbroersma.nl>
2024-12-31 17:13:03 +0000

    Deploying to gh-pages from @ nl-design-system/rotterdam@716a41e7294cfdcb67e777955a328d691b746b70 🚀

---
+++ assets/css-denhaag-action.stories-CQijAomA.js
@@ -0,0 +1,19 @@
+import{j as r}from"./jsx-runtime-C8prxwi0.js";import{A as o}from"./index-DitNJaCd.js";import"./index-DkGvG02X.js";import"./_commonjsHelpers-CqkleIqs.js";const i=`<!-- @license CC0-1.0 -->
+
+# Action
+`,g={id:"css-denhaag-action",args:{defaultValue:""},argTypes:{defaultValue:{name:"Content",defaultValue:"",type:{name:"string",required:!0}}},component:o,parameters:{design:{type:"figma",url:"https://www.figma.com/file/hQPGq7PNYv3RvBdzSeqKf2/Mijn-Loket-2.0?node-id=810%3A3078"},docs:{description:{component:i}}},tags:["autodocs"],title:"CSS Component/Action"},e={name:"Single action",args:{children:r.jsx("strong",{children:"Betaal uw parkeerbon van €68,90"}),dateTime:"2023-09-30T19:47:36.593Z",labels:{approachingDeadline:()=>"nog 2 dagen",before:"vóór",today:"vandaag",yesterday:"gisteren"},link:"#",now:"2023-09-28T19:47:36.593Z",relativeDate:!0}};var n,a,t;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  name: 'Single action',
+  args: {
+    children: <strong>Betaal uw parkeerbon van €68,90</strong>,
+    dateTime: '2023-09-30T19:47:36.593Z',
+    labels: {
+      approachingDeadline: () => 'nog 2 dagen',
+      before: 'vóór',
+      today: 'vandaag',
+      yesterday: 'gisteren'
+    },
+    link: '#',
+    now: '2023-09-28T19:47:36.593Z',
+    relativeDate: true
+  }
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const m=["Default"];export{e as Default,m as __namedExportsOrder,g as default};
---
+++ assets/css-utrecht-button-group.stories-BcUZe7_4.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-C8prxwi0.js";import{h as s,c as n,i as u}from"./index-CdS-qCIo.js";import"./index-DkGvG02X.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BPJg34WK.js";const c=`<!-- @license CC0-1.0 -->
+
+# Button group
+`,g={id:"css-utrecht-button-group",args:{children:""},argTypes:{children:{name:"Content",defaultValue:"",type:{name:"string",required:!0}}},component:s,parameters:{bugs:"https://github.com/nl-design-system/rotterdam/labels/component%2Fbutton-group",design:{type:"figma",url:"https://www.figma.com/design/iWuQsxelJzXVfCge87Og81/RODS-component?node-id=1-56&t=EXoPmctpkctdqa4z-4"},docs:{description:{component:c}}},tags:["autodocs"],title:"CSS Component/Button group"},e={name:"Default",args:{children:t.jsxs(t.Fragment,{children:[t.jsx(n,{appearance:"primary-action-button",children:"Primary"}),t.jsx(n,{appearance:"secondary-action-button",children:"Label"}),t.jsx(n,{children:"Label"}),t.jsx(u,{children:"Text button"})]})}};var o,r,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  name: 'Default',
+  args: {
+    children: <>
+        <Button appearance="primary-action-button">Primary</Button>
+        <Button appearance="secondary-action-button">Label</Button>
+        <Button>Label</Button>
+        <LinkButton>Text button</LinkButton>
+      </>
+  }
+}`,...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const b=["Default"];export{e as Default,b as __namedExportsOrder,g as default};
