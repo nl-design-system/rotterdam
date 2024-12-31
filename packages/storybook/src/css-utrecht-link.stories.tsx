7eec19b0bae88a983f9f8de6f2d55a5ebce218bf
Robbert Broersma <git@robbertbroersma.nl>
2024-12-31 15:49:48 +0100

    refactor: fix typescript warnings

--- packages/storybook/src/css-utrecht-link.stories.tsx
+++ packages/storybook/src/css-utrecht-link.stories.tsx
@@ -1,8 +1,21 @@
 /* @license CC0-1.0 */
 
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Decorator, Meta, StoryObj } from '@storybook/react';
 import { Link, Paragraph } from '@utrecht/component-library-react/dist/css-module';
 import readme from './link.md?raw';
+
+const ParagraphContentDecorator: Decorator = (Story) => (
+  <Paragraph>
+    Een geveltuin aanleggen is leuk om te doen, de straat knapt ervan op en uw huis blijft in de zomer koeler. Meer
+    groen maakt de stad ook beter bestand tegen klimaatveranderingen en wateroverlast en is goed voor insecten en
+    vogels. {Story()} Ze zijn vooral geschikt in straten waar de huizen geen voortuin hebben. Eén enkele klimplant,
+    stokroos of zonnebloem maakt het straatbeeld al gezelliger, maar andere planten kunnen natuurlijk ook. U kiest zelf.
+  </Paragraph>
+);
+
+const LeadParagraphDecorator: Decorator = (Story) => <Paragraph appearance="lead">{Story()}</Paragraph>;
+
+const SmallParagraphDecorator: Decorator = (Story) => <Paragraph appearance="small">{Story()}</Paragraph>;
 
 const meta = {
   id: 'css-utrecht-link',
@@ -53,17 +66,7 @@ export const LinkInParagraph: Story = {
     children: 'Een geveltuin is een strook planten tegen de gevel van een woning.',
     href: '#',
   },
-  decorators: [
-    (Story) => (
-      <Paragraph>
-        Een geveltuin aanleggen is leuk om te doen, de straat knapt ervan op en uw huis blijft in de zomer koeler. Meer
-        groen maakt de stad ook beter bestand tegen klimaatveranderingen en wateroverlast en is goed voor insecten en
-        vogels. {Story()} Ze zijn vooral geschikt in straten waar de huizen geen voortuin hebben. Eén enkele klimplant,
-        stokroos of zonnebloem maakt het straatbeeld al gezelliger, maar andere planten kunnen natuurlijk ook. U kiest
-        zelf.
-      </Paragraph>
-    ),
-  ],
+  decorators: [ParagraphContentDecorator],
 };
 
 export const LinkInLeadParagraph: Story = {
@@ -72,7 +75,7 @@ export const LinkInLeadParagraph: Story = {
     children: 'Een geveltuin aanleggen',
     href: '#',
   },
-  decorators: [(Story) => <Paragraph lead>{Story()}</Paragraph>],
+  decorators: [LeadParagraphDecorator],
 };
 
 export const LinkInSmallPrint: Story = {
@@ -81,5 +84,5 @@ export const LinkInSmallPrint: Story = {
     children: 'Een geveltuin aanleggen',
     href: '#',
   },
-  decorators: [(Story) => <Paragraph small>{Story()}</Paragraph>],
+  decorators: [SmallParagraphDecorator],
 };
