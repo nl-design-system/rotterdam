7c88ad786b57d2ff16c77f0b9e12d1516e470988
Robbert <mail@robbertbroersma.nl>
2025-01-01 15:33:47 +0000

    Deploying to gh-pages from @ nl-design-system/rotterdam@8fc9204a7c739542191b31c299a34e0d933502bb 🚀

---
+++ assets/form-field-checkbox-WFJT3yph.js
@@ -0,0 +1,38 @@
+import{j as e}from"./jsx-runtime-C8prxwi0.js";import{useMDXComponents as r}from"./index-BVXH1tlS.js";import{ap as i,aq as c}from"./index-zTh-TmpC.js";import"./index-DkGvG02X.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-BVtoE53k.js";import"../sb-preview/runtime.js";import"./index-DmLoi2Vx.js";import"./index-CHGET4sZ.js";const d=`<!-- @license CC0-1.0 -->
+
+# Form Field Checkbox component voor Apache Wicket
+
+Formulierveld om te kiezen tussen "wel" of "niet".
+
+
+[Broncode van dit voorbeeld](https://github.com/nl-design-system/rotterdam/blob/main/wicket/docs-wicket//src/main/java/nl/rotterdam/wicket/docs/form_field_checkbox/)
+
+[Broncode van de component](https://github.com/nl-design-system/rotterdam/blob/main/wicket/components-wicket//src/main/java/nl/rotterdam/wicket/docs/form_field_checkbox/)
+
+## Form Field Checkbox
+
+Voorbeeld.
+
+
+\`\`\`html
+<div wicket:id="utrechtFormFieldCheckbox"></div>
+\`\`\`
+
+\`\`\`java
+public class ExamplePanel extends Panel {
+
+    public ExamplePanel(String id) {
+        super(id);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        add(new UtrechtFormFieldCheckbox("utrechtFormFieldCheckbox", Model.of(false), Model.of("I agree"), Model.of("Description"), Model.of("error")));
+    }
+}
+\`\`\`
+`;/*@license CC0-1.0*/function t(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{title:"Apache Wicket/Form Field Checkbox"}),`
+`,e.jsx(c,{children:d})]})}function u(n={}){const{wrapper:o}={...r(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(t,{...n})}):t()}export{u as default};
