47596bde3c54b601ec641c79c49e90d245d678c9
Kees van Dieren <kees@squins.com>
2025-09-05 16:17:00 +0200

    #142 change needed added related TODO

--- wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/badge_list/utrecht/UtrechtBadgeListBorder.java
+++ wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/badge_list/utrecht/UtrechtBadgeListBorder.java
@@ -2,10 +2,13 @@ package nl.rotterdam.design_system.wicket.components.badge_list.utrecht;
 
 import org.apache.wicket.markup.html.border.Border;
 
+
+// TODO utrecht-badge-list will be added twice
+@Deprecated
 public class UtrechtBadgeListBorder extends Border {
 
     public UtrechtBadgeListBorder(String id) {
         super(id);
-        add(new UtrechtBadgeListBehavior());
+        add(UtrechtBadgeListBehavior.INSTANCE);
     }
 }
