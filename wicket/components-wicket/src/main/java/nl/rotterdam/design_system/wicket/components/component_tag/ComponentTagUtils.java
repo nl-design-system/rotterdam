febd5154ee22708db2b4e6423fdfa22a207526a3
Kees van Dieren <kees@squins.com>
2025-09-26 14:08:23 +0200

    #202 explain why deprecated

--- wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/component_tag/ComponentTagUtils.java
+++ wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/component_tag/ComponentTagUtils.java
@@ -4,6 +4,12 @@ import org.apache.wicket.markup.ComponentTag;
 
 public enum ComponentTagUtils {
     ;
+
+    /**
+     * @since 25-sep-2025
+     * @deprecated use {@link nl.rotterdam.design_system.wicket.components.css_class_names.CssClassNames} with an
+     * enum instead. Use of CSS literals is discouraged.
+     */
     @Deprecated
     public static void appendCssClassesTo(ComponentTag tag, String cssClasses) {
         tag.append("class", cssClasses, " ");
