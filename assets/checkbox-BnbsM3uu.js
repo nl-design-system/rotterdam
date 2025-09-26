08f961012a0788bc5e33517db6aa14771f27905a
keesvandieren <keesvandieren@squins.com>
2025-09-26 14:45:20 +0000

    Deploying to gh-pages from @ nl-design-system/rotterdam@a1d38e89b188eae61b7da498afb5d74014e6855e 🚀

---
+++ assets/checkbox-BnbsM3uu.js
@@ -0,0 +1,76 @@
+import{j as n}from"./jsx-runtime-B7cIiniC.js";import{useMDXComponents as i}from"./index-DbGdhrAm.js";import{ap as r,aq as c}from"./index-BWtC5L88.js";import"./index-DCEVbkbO.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-1qIRjLkf.js";import"../sb-preview/runtime.js";import"./index-DYTvpxhr.js";import"./index-CXQShRbs.js";const a=`<!-- @license CC0-1.0 -->
+
+# Checkbox component voor Apache Wicket
+
+Invoerveld voor het kiezen van een optie uit een groep van opties, of om te kiezen tussen "wel" of "niet".
+
+Zie ook: Form Field Checkbox
+
+
+[Broncode van dit voorbeeld](https://github.com/nl-design-system/rotterdam/blob/main/wicket/docs-wicket//src/main/java/nl/rotterdam/wicket/docs/checkbox/)
+
+[Broncode van de component](https://github.com/nl-design-system/rotterdam/blob/main/wicket/components-wicket//src/main/java/nl/rotterdam/wicket/docs/checkbox/)
+
+## Checkbox
+
+Voorbeeld standaard checkbox.
+
+
+\`\`\`html
+<input wicket:id="checkbox" type="checkbox">
+\`\`\`
+
+\`\`\`java
+public class ExamplePanel extends Panel {
+
+    public ExamplePanel(String id) {
+        super(id);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        add(createComponent());
+    }
+    
+    private Component createComponent() {
+        return new UtrechtCheckbox("checkbox", Model.of(false));
+    }
+
+}
+\`\`\`
+
+## Checkbox Invalid
+
+Voorbeeld checkbox die door gebruiker verzonden is, maar niet is ingevuld.
+
+
+\`\`\`html
+<input wicket:id="checkboxInvalid" type="checkbox">
+\`\`\`
+
+\`\`\`java
+public class ExamplePanel extends Panel {
+
+    public ExamplePanel(String id) {
+        super(id);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        add(createComponent());
+    }
+    
+    private Component createComponent() {
+        CheckBox component = new UtrechtCheckbox("checkboxInvalid", Model.of(false));
+        component.error("Melding, wordt bij standalone Checkbox niet getoond, wel is het veld rood");
+        return component;
+    }
+
+}
+\`\`\`
+`;/*@license CC0-1.0*/function t(e){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{title:"Apache Wicket/Checkbox"}),`
+`,n.jsx(c,{children:a})]})}function u(e={}){const{wrapper:o}={...i(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(t,{...e})}):t()}export{u as default};
---
+++ assets/heading-group-5L3-YcU_.js
@@ -0,0 +1,46 @@
+import{j as n}from"./jsx-runtime-B7cIiniC.js";import{useMDXComponents as i}from"./index-DbGdhrAm.js";import{ap as r,aq as a}from"./index-BWtC5L88.js";import"./index-DCEVbkbO.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-1qIRjLkf.js";import"../sb-preview/runtime.js";import"./index-DYTvpxhr.js";import"./index-CXQShRbs.js";const d=`<!-- @license CC0-1.0 -->
+
+# Heading Group component voor Apache Wicket
+
+Toont een combinatie van een titel en ondersteunende tekst.
+
+
+[Broncode van dit voorbeeld](https://github.com/nl-design-system/rotterdam/blob/main/wicket/docs-wicket//src/main/java/nl/rotterdam/wicket/docs/heading_group/)
+
+[Broncode van de component](https://github.com/nl-design-system/rotterdam/blob/main/wicket/components-wicket//src/main/java/nl/rotterdam/wicket/docs/heading_group/)
+
+## Heading Group
+
+Voorbeeld.
+
+
+\`\`\`html
+<div wicket:id="utrechtHeadingGroup">
+ <h2>Heading in a heading group</h2>
+ <p>Sub-heading</p>
+</div>
+\`\`\`
+
+\`\`\`java
+public class ExamplePanel extends Panel {
+
+    public ExamplePanel(String id) {
+        super(id);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        add(createComponent());
+    }
+    
+    private Component createComponent() {
+        return new UtrechtHeadingGroupBorder("utrechtHeadingGroup");
+    }
+
+}
+\`\`\`
+`;/*@license CC0-1.0*/function o(e){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(r,{title:"Apache Wicket/Heading Group"}),`
+`,n.jsx(a,{children:d})]})}function b(e={}){const{wrapper:t}={...i(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o()}export{b as default};
