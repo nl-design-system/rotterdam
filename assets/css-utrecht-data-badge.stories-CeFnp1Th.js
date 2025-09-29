ee0297407784c36f105c51d9bc0be8414247c549
keesvandieren <keesvandieren@squins.com>
2025-09-29 15:25:52 +0000

    Deploying to gh-pages from @ nl-design-system/rotterdam@11fc2c11b2d553a31185e9c3c053663a1a149e9d 🚀

---
+++ assets/css-utrecht-data-badge.stories-CeFnp1Th.js
@@ -0,0 +1,128 @@
+import{j as x}from"./jsx-runtime-B7cIiniC.js";import{D as I,L as K}from"./index-Dl7OJaKU.js";import"./index-DCEVbkbO.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-8Pb4k3Ki.js";const L=`<!-- @license CC0-1.0 -->
+
+# Data badge
+
+These type of tags can be used on a page where you want the user to show which themes or subjects the page is part of. For example a project that is part of the themes ‘sustainability’ and ‘clean & safe’. These tags are clickable and lead to a page where all projects with the same theme are shown.
+`,X=C=>x.jsx(K,{href:"#",children:C()}),q={id:"css-utrecht-data-badge",args:{children:""},argTypes:{children:{name:"Content",defaultValue:"",description:"Data badge text",type:{name:"string",required:!0}}},component:I,parameters:{bugs:"https://github.com/nl-design-system/rotterdam/labels/component%2Fdata-badge",design:{type:"figma",url:"https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/?node-id=20736-12320"},docs:{description:{component:L}}},tags:["autodocs"],title:"CSS Component/Data badge"},e={name:"Default",args:{children:"sustainability"},parameters:{design:{type:"figma",url:"https://www.figma.com/design/iWuQsxelJzXVfCge87Og81/RODS-component?node-id=1-5876&t=EXoPmctpkctdqa4z-4"}}},s={name:"Badge as link",args:{children:"sustainability"},decorators:[X],parameters:{design:{type:"figma",url:"https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/?node-id=8202-9795"},status:{type:"ALPHA"}}},a={name:"Light blue",args:{children:"sustainability",className:"utrecht-data-badge--rods-light-blue"},parameters:{docs:{description:{story:"Do not use: this is not part of the Figma designs yet."}},status:{type:"WORK IN PROGRESS"}}},t={name:"Blue",args:{children:"sustainability",className:"utrecht-data-badge--rods-blue"},parameters:{docs:{description:{story:"Do not use: this is not part of the Figma designs yet."}},status:{type:"WORK IN PROGRESS"}}},n={name:"Dark blue",args:{children:"sustainability",className:"utrecht-data-badge--rods-dark-blue"},parameters:{docs:{description:{story:"Do not use: this is not part of the Figma designs yet."}},status:{type:"WORK IN PROGRESS"}}},r={name:"Green blue",args:{children:"sustainability",className:"utrecht-data-badge--rods-green-blue"},parameters:{docs:{description:{story:"Do not use: this is not part of the Figma designs yet."}},status:{type:"WORK IN PROGRESS"}}},o={name:"Red",args:{children:"sustainability",className:"utrecht-data-badge--rods-red"},parameters:{docs:{description:{story:"Do not use: this is not part of the Figma designs yet."}},status:{type:"WORK IN PROGRESS"}}},i={name:"Dark yellow",args:{children:"sustainability",className:"utrecht-data-badge--rods-dark-yellow"},parameters:{docs:{description:{story:"Do not use: this is not part of the Figma designs yet."}},status:{type:"WORK IN PROGRESS"}}};var d,c,m;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  name: 'Default',
+  args: {
+    children: 'sustainability'
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/iWuQsxelJzXVfCge87Og81/RODS-component?node-id=1-5876&t=EXoPmctpkctdqa4z-4'
+    }
+  }
+}`,...(m=(c=e.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var u,p,l;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  name: 'Badge as link',
+  args: {
+    children: 'sustainability'
+  },
+  decorators: [LinkDecorator],
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/ZWSC4gCrOXRUR9UX3aoZ8x/?node-id=8202-9795'
+    },
+    status: {
+      type: 'ALPHA'
+    }
+  }
+}`,...(l=(p=s.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var g,h,y;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  name: 'Light blue',
+  args: {
+    children: 'sustainability',
+    className: 'utrecht-data-badge--rods-light-blue'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Do not use: this is not part of the Figma designs yet.'
+      }
+    },
+    status: {
+      type: 'WORK IN PROGRESS'
+    }
+  }
+}`,...(y=(h=a.parameters)==null?void 0:h.docs)==null?void 0:y.source}}};var b,R,f;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  name: 'Blue',
+  args: {
+    children: 'sustainability',
+    className: 'utrecht-data-badge--rods-blue'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Do not use: this is not part of the Figma designs yet.'
+      }
+    },
+    status: {
+      type: 'WORK IN PROGRESS'
+    }
+  }
+}`,...(f=(R=t.parameters)==null?void 0:R.docs)==null?void 0:f.source}}};var S,D,O;n.parameters={...n.parameters,docs:{...(S=n.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  name: 'Dark blue',
+  args: {
+    children: 'sustainability',
+    className: 'utrecht-data-badge--rods-dark-blue'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Do not use: this is not part of the Figma designs yet.'
+      }
+    },
+    status: {
+      type: 'WORK IN PROGRESS'
+    }
+  }
+}`,...(O=(D=n.parameters)==null?void 0:D.docs)==null?void 0:O.source}}};var w,N,k;r.parameters={...r.parameters,docs:{...(w=r.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  name: 'Green blue',
+  args: {
+    children: 'sustainability',
+    className: 'utrecht-data-badge--rods-green-blue'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Do not use: this is not part of the Figma designs yet.'
+      }
+    },
+    status: {
+      type: 'WORK IN PROGRESS'
+    }
+  }
+}`,...(k=(N=r.parameters)==null?void 0:N.docs)==null?void 0:k.source}}};var W,E,G;o.parameters={...o.parameters,docs:{...(W=o.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  name: 'Red',
+  args: {
+    children: 'sustainability',
+    className: 'utrecht-data-badge--rods-red'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Do not use: this is not part of the Figma designs yet.'
+      }
+    },
+    status: {
+      type: 'WORK IN PROGRESS'
+    }
+  }
+}`,...(G=(E=o.parameters)==null?void 0:E.docs)==null?void 0:G.source}}};var P,B,F;i.parameters={...i.parameters,docs:{...(P=i.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  name: 'Dark yellow',
+  args: {
+    children: 'sustainability',
+    className: 'utrecht-data-badge--rods-dark-yellow'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Do not use: this is not part of the Figma designs yet.'
+      }
+    },
+    status: {
+      type: 'WORK IN PROGRESS'
+    }
+  }
+}`,...(F=(B=i.parameters)==null?void 0:B.docs)==null?void 0:F.source}}};const T=["Default","BadgeAsLink","LightBlue","Blue","DarkBlue","GreenBlue","Red","DarkYellow"];export{s as BadgeAsLink,t as Blue,n as DarkBlue,i as DarkYellow,e as Default,r as GreenBlue,a as LightBlue,o as Red,T as __namedExportsOrder,q as default};
---
+++ assets/form-field-checkbox-BjSztAwM.js
@@ -0,0 +1,74 @@
+import{j as e}from"./jsx-runtime-B7cIiniC.js";import{useMDXComponents as r}from"./index-DbGdhrAm.js";import{ap as i,aq as a}from"./index-Cl-lmMzQ.js";import"./index-DCEVbkbO.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-c3Dmc2oz.js";import"../sb-preview/runtime.js";import"./index-DYTvpxhr.js";import"./index-CXQShRbs.js";const d=`<!-- @license CC0-1.0 -->
+
+# Form Field Checkbox component voor Apache Wicket
+
+Formulierveld om te kiezen tussen "wel" of "niet".
+
+
+[Broncode van dit voorbeeld](https://github.com/nl-design-system/rotterdam/blob/main/wicket/docs-wicket//src/main/java/nl/rotterdam/wicket/docs/form_field_checkbox/)
+
+[Broncode van de component](https://github.com/nl-design-system/rotterdam/blob/main/wicket/components-wicket//src/main/java/nl/rotterdam/wicket/docs/form_field_checkbox/)
+
+## Form Field Checkbox
+
+Voorbeeld.
+
+
+\`\`\`html
+<div wicket:id="utrechtFormFieldCheckbox"></div>
+\`\`\`
+
+\`\`\`java
+public class ExamplePanel extends Panel {
+
+    public ExamplePanel(String id) {
+        super(id);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        add(createComponent());
+    }
+    
+    private Component createComponent() {
+        return new UtrechtFormFieldCheckbox("utrechtFormFieldCheckbox", Model.of(false), Model.of("Ik ga akkoord met de algemene voorwaarden"), Model.of());
+    }
+
+}
+\`\`\`
+
+## Form Field Checkbox Invalid
+
+Voorbeeld.
+
+
+\`\`\`html
+<div wicket:id="utrechtFormFieldCheckboxInvalid"></div>
+\`\`\`
+
+\`\`\`java
+public class ExamplePanel extends Panel {
+
+    public ExamplePanel(String id) {
+        super(id);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        add(createComponent());
+    }
+    
+    private Component createComponent() {
+        UtrechtFormFieldCheckbox component = new UtrechtFormFieldCheckbox("utrechtFormFieldCheckboxInvalid", Model.of(false), Model.of("Ik ga akkoord met de algemene voorwaarden"), Model.of("U kunt ons echt vertrouwen!"));
+        component.getControl().error("U moet akkoord gaan met de algemene voorwaarden, anders kunt u niet verder.");
+        return component;
+    }
+
+}
+\`\`\`
+`;/*@license CC0-1.0*/function t(n){return e.jsxs(e.Fragment,{children:[`
+`,`
+`,e.jsx(i,{title:"Apache Wicket/Form Field Checkbox"}),`
+`,e.jsx(a,{children:d})]})}function v(n={}){const{wrapper:o}={...r(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(t,{...n})}):t()}export{v as default};
