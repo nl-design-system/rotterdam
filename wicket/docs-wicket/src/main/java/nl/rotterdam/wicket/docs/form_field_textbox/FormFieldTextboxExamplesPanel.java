b150ee2ab3241d73261c74f61436ee070321e1fc
Kees van Dieren <kees@squins.com>
2025-09-30 17:30:41 +0200

    #252 rename examples

--- wicket/docs-wicket/src/main/java/nl/rotterdam/wicket/docs/form_field_textbox/FormFieldTextboxExamplesPanel.java
+++ wicket/docs-wicket/src/main/java/nl/rotterdam/wicket/docs/form_field_textbox/FormFieldTextboxExamplesPanel.java
@@ -1,42 +1,43 @@
 package nl.rotterdam.wicket.docs.form_field_textbox;
 
 import nl.rotterdam.design_system.wicket.components.form_field_textbox.NlFormFieldTextbox;
+import nl.rotterdam.design_system.wicket.components.models.DefaultModels;
 import nl.rotterdam.wicket.docs.ComponentExample;
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 
-public class UtrechtFormFieldTextboxExamplesPanel extends Panel {
+public class FormFieldTextboxExamplesPanel extends Panel {
 
-    public UtrechtFormFieldTextboxExamplesPanel(String id) {
+    public FormFieldTextboxExamplesPanel(String id) {
         super(id);
     }
 
     @ComponentExample
     private static Component exampleFormFieldTextbox() {
         return new NlFormFieldTextbox<>(
-            "utrechtFormFieldTextbox",
-            Model.of("Hello world!"),
-            Model.of("I agree"),
-            Model.of("Description")
+            "nlFormFieldTextbox",
+            Model.of("Koen"),
+            Model.of("Voornaam"),
+            Model.of("Vul je voornaam in.")
         );
     }
 
     @ComponentExample
     private static Component exampleFormFieldTextboxRequired() {
         return new NlFormFieldTextbox<>(
-            "utrechtFormFieldTextboxRequired",
-            Model.of("Hello world!"),
-            Model.of("I agree"),
-            Model.of("Description")
+            "nlFormFieldTextboxRequired",
+            DefaultModels.EMPTY_STRING_MODEL,
+            Model.of("Email")
         )
-            .setRequired(true);
+        .setInputType("email")
+        .setRequired(true);
     }
 
     @ComponentExample
     private static Component exampleFormFieldTextboxDisabled() {
         return new NlFormFieldTextbox<>(
-            "utrechtFormFieldTextboxDisabled",
+            "nlFormFieldTextboxDisabled",
             Model.of("Hello world!"),
             Model.of("I agree"),
             Model.of("Description")
