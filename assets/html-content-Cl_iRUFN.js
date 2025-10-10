e9f6f55a3e250a2ee901618e05ce2b3b89cf7bd2
keesvandieren <keesvandieren@squins.com>
2025-10-10 15:08:52 +0000

    Deploying to gh-pages from @ nl-design-system/rotterdam@a37317d09f7174932d61125aecd2b3b23787fd6e 🚀

---
+++ assets/html-content-Cl_iRUFN.js
@@ -0,0 +1,48 @@
+import{j as n}from"./jsx-runtime-B7cIiniC.js";import{useMDXComponents as r}from"./index-DbGdhrAm.js";import{ap as i,aq as m}from"./index-DM1K1-lq.js";import"./index-DCEVbkbO.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-C8GJd0H2.js";import"../sb-preview/runtime.js";import"./index-DYTvpxhr.js";import"./index-CXQShRbs.js";const a=`<!-- @license CC0-1.0 -->
+
+# HTML Content component voor Apache Wicket
+
+Toont HTML-inhoud met de opmaak van het design system.
+
+
+[Broncode van dit voorbeeld](https://github.com/nl-design-system/rotterdam/blob/main/wicket/docs-wicket//src/main/java/nl/rotterdam/wicket/docs/html_content/)
+
+[Broncode van de component](https://github.com/nl-design-system/rotterdam/blob/main/wicket/components-wicket//src/main/java/nl/rotterdam/wicket/docs/html_content/)
+
+## HTML Content
+
+Voorbeeld.
+
+
+\`\`\`html
+<div wicket:id="utrechtHtmlContent">
+ <h1>Hello world</h1>
+ <p>
+  <button type="submit">Submit</button>
+ </p>
+</div>
+\`\`\`
+
+\`\`\`java
+public class ExamplePanel extends Panel {
+
+    public ExamplePanel(String id) {
+        super(id);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        add(createComponent());
+    }
+    
+    private Component createComponent() {
+        return new UtrechtHtmlContentBorder("utrechtHtmlContent");
+    }
+
+}
+\`\`\`
+`;/*@license CC0-1.0*/function o(t){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(i,{title:"Apache Wicket/HTML Content"}),`
+`,n.jsx(m,{children:a})]})}function v(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o()}export{v as default};
---
+++ assets/utrecht-separator-DTXCEY_5.js
@@ -0,0 +1,43 @@
+import{j as n}from"./jsx-runtime-B7cIiniC.js";import{useMDXComponents as o}from"./index-DbGdhrAm.js";import{ap as a,aq as i}from"./index-DM1K1-lq.js";import"./index-DCEVbkbO.js";import"./_commonjsHelpers-CqkleIqs.js";import"./iframe-C8GJd0H2.js";import"../sb-preview/runtime.js";import"./index-DYTvpxhr.js";import"./index-CXQShRbs.js";const c=`<!-- @license CC0-1.0 -->
+
+# Separator component voor Apache Wicket
+
+Markering van de plek waar de tekst op een nieuwe manier verdergaat, zoals een nieuwe scène of een ander onderwerp.
+
+
+[Broncode van dit voorbeeld](https://github.com/nl-design-system/rotterdam/blob/main/wicket/docs-wicket//src/main/java/nl/rotterdam/wicket/docs/separator/)
+
+[Broncode van de component](https://github.com/nl-design-system/rotterdam/blob/main/wicket/components-wicket//src/main/java/nl/rotterdam/wicket/docs/separator/)
+
+## Separator
+
+Voorbeeld.
+
+
+\`\`\`html
+<div wicket:id="utrechtSeparator"></div>
+\`\`\`
+
+\`\`\`java
+public class ExamplePanel extends Panel {
+
+    public ExamplePanel(String id) {
+        super(id);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        add(createComponent());
+    }
+    
+    private Component createComponent() {
+        return new UtrechtSeparator("utrechtSeparator");
+    }
+
+}
+\`\`\`
+`;/*@license CC0-1.0*/function r(e){return n.jsxs(n.Fragment,{children:[`
+`,`
+`,n.jsx(a,{title:"Apache Wicket/Separator"}),`
+`,n.jsx(i,{children:c})]})}function h(e={}){const{wrapper:t}={...o(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(r,{...e})}):r()}export{h as default};
