3c4ac41bfe314cbf58beb092ca74c024f9baf959
Kees van Dieren <kees@squins.com>
2025-10-16 11:03:20 +0200

    #129 must be serializable

--- wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/data_summary/SummaryItem.java
+++ wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/data_summary/SummaryItem.java
@@ -1,11 +1,10 @@
 package nl.rotterdam.design_system.wicket.components.data_summary;
 
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
 
 import java.io.Serializable;
 
-public record SummaryItem(String label, SummaryItemValue value) {
+public record SummaryItem(String label, SummaryItemValue value) implements Serializable {
 
     public SummaryItem(IModel<String> label, SummaryItemValue value) {
         this(label.getObject(), value);
