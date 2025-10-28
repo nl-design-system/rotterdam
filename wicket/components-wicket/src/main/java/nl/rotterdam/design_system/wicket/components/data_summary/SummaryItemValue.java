af1e8505c7d27524e7a0bb06aab6972dd27e01a5
Kees van Dieren <kees@squins.com>
2025-10-28 21:07:23 +0100

    Needs to be Serializable
    As it might be used in a non-ldm model

--- wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/data_summary/SummaryItemValue.java
+++ wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/data_summary/SummaryItemValue.java
@@ -2,6 +2,6 @@ package nl.rotterdam.design_system.wicket.components.data_summary;
 
 import java.io.Serializable;
 
-public record SummaryItemValue( Serializable value, boolean translate) {
+public record SummaryItemValue( Serializable value, boolean translate) implements Serializable {
 
 }
