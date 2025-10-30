825073284bbcd1dc67baca5bf40da7262c39b0ec
Kees van Dieren <kees@squins.com>
2025-10-30 17:06:19 +0100

    Partial moved file, now really deleted from old location.

--- wicket/components-wicket/src/main/java/nl/rotterdam/design_system/wicket/components/data_summary/SummaryItemValue.java
+++
@@ -1,7 +0,0 @@
-package nl.rotterdam.design_system.wicket.components.data_summary;
-
-import java.io.Serializable;
-
-public record SummaryItemValue( Serializable value, boolean translate) implements Serializable {
-
-}
