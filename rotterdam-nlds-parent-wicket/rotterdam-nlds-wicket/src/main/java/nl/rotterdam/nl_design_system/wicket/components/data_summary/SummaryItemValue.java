87b1707fdfd19fb565470c23526e16520f309e91
Johan Stuyts <j.stuyts@javathinker.com>
2025-10-30 17:18:35 +0100

    #316 gecontroleerd op beveiligingsproblemen en (waar mogelijk) opgelost. (#317)
    Co-authored-by: Johan Stuyts <j.stuyts@rotterdam.nl>
    Co-authored-by: Kees van Dieren <kees@squins.com>

--- rotterdam-nlds-parent-wicket/rotterdam-nlds-wicket/src/main/java/nl/rotterdam/nl_design_system/wicket/components/data_summary/SummaryItemValue.java
+++ rotterdam-nlds-parent-wicket/rotterdam-nlds-wicket/src/main/java/nl/rotterdam/nl_design_system/wicket/components/data_summary/SummaryItemValue.java
@@ -2,4 +2,4 @@ package nl.rotterdam.nl_design_system.wicket.components.data_summary;
 
 import java.io.Serializable;
 
-public record SummaryItemValue(Serializable value, boolean translate) {}
+public record SummaryItemValue(Serializable value, boolean translate) implements Serializable {}
