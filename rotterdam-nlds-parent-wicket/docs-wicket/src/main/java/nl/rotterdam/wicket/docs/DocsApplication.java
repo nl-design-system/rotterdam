eb4752d937a63c14d5350b58ec3c74c0fd53b87f
Johan Stuyts <j.stuyts@rotterdam.nl>
2025-11-05 18:10:19 +0100

    #314 headercontributor toegevoegd die Wicket-tags transparant voor layouts maakt.

--- rotterdam-nlds-parent-wicket/docs-wicket/src/main/java/nl/rotterdam/wicket/docs/DocsApplication.java
+++ rotterdam-nlds-parent-wicket/docs-wicket/src/main/java/nl/rotterdam/wicket/docs/DocsApplication.java
@@ -1,5 +1,6 @@
 package nl.rotterdam.wicket.docs;
 
+import nl.rotterdam.nl_design_system.wicket.html.WicketElementDisplayContentsHeaderContributor;
 import nl.rotterdam.wicket.docs.mijn_loket_page.MijnLoketPage;
 import nl.rotterdam.wicket.docs.sso_login_page.SingleSignOnLoginPage;
 import org.apache.wicket.csp.CSPDirective;
@@ -14,8 +15,9 @@ public class DocsApplication extends WebApplication {
     public void init() {
         super.init();
 
-        // Avoid <wicket:panel> in the output HTML
-        getMarkupSettings().setStripWicketTags(true);
+        if (usesDevelopmentConfig()) {
+            getHeaderContributorListeners().add(WicketElementDisplayContentsHeaderContributor.INSTANCE);
+        }
 
         // Configure our own fance error page
         getExceptionSettings().setUnexpectedExceptionDisplay(ExceptionSettings.SHOW_EXCEPTION_PAGE);
