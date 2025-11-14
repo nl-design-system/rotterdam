d4e87c9af6c12c67aec4af64275b2b0c25d7b5ef
Johan Stuyts <j.stuyts@javathinker.com>
2025-11-14 15:02:16 +0100

    #350 Module-informatie toegevoegd. Conflicterend package hernoemd. (#357)
    Closes #350.
    
    ---------
    
    Co-authored-by: Johan Stuyts <j.stuyts@rotterdam.nl>

---
+++ rotterdam-nlds-parent-wicket/rotterdam-nlds-extensions-wicket/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module nl.rotterdam.nlds.extensions.wicket {
+    exports nl.rotterdam.nl_design_system.rotterdam_extensions.wicket.components.rotterdam_icon;
+    exports nl.rotterdam.nl_design_system.rotterdam_extensions.wicket.components.rotterdam_logo;
+
+    opens nl.rotterdam.nl_design_system.rotterdam_extensions.wicket.components.rotterdam_icon;
+    opens nl.rotterdam.nl_design_system.rotterdam_extensions.wicket.components.rotterdam_logo;
+
+    requires nl.rotterdam.nlds.wicket;
+    requires org.apache.wicket.core;
+}
