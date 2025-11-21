7698467cc884f99281e08b25b1d6387a7424be01
Kees van Dieren <kees@squins.com>
2025-11-21 11:56:48 +0100

    Removed intermediate 'nav' layer.
    Add nav tag when using component

--- rotterdam-nlds-parent-wicket/rotterdam-nlds-wicket/src/main/java/nl/rotterdam/nl_design_system/wicket/components/side_nav/RdSideNavPanel.java
+++ rotterdam-nlds-parent-wicket/rotterdam-nlds-wicket/src/main/java/nl/rotterdam/nl_design_system/wicket/components/side_nav/RdSideNavPanel.java
@@ -2,6 +2,7 @@ package nl.rotterdam.nl_design_system.wicket.components.side_nav;
 
 import nl.rotterdam.nl_design_system.wicket.components.component_state.NlComponentState;
 import nl.rotterdam.nl_design_system.wicket.components.number_badge.RdNumberBadge;
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
@@ -14,6 +15,7 @@ import java.util.List;
 import static nl.rotterdam.nl_design_system.wicket.components.component_state.Community.UTRECHT;
 import static nl.rotterdam.nl_design_system.wicket.components.component_state.EstafetteState.COMMUNITY;
 import static nl.rotterdam.nl_design_system.wicket.components.component_state.WicketState.NEEDS_REFACTORING;
+import static nl.rotterdam.nl_design_system.wicket.components.output_tag.ComponentTagAssertions.assertIsRegularHtmlTag;
 import static nl.rotterdam.nl_design_system.wicket.components.side_nav.RdSideNavListBehavior.SIDE_NAV_LIST_BEHAVIOR;
 
 /**
@@ -26,9 +28,10 @@ public class RdSideNavPanel extends Panel {
 
     /**
      * Create new instance
-     * @param id the Wicket ID
+     *
+     * @param id             the Wicket ID
      * @param sideNavRecords records to add
-     *                       TODO refactor to model.
+     *                                             TODO refactor to model.
      */
     public RdSideNavPanel(String id, List<RdSideNavRecord<? extends IRequestablePage>> sideNavRecords) {
         super(id);
@@ -36,65 +39,63 @@ public class RdSideNavPanel extends Panel {
     }
 
     @Override
+    protected void onComponentTag(ComponentTag tag) {
+        super.onComponentTag(tag);
+        assertIsRegularHtmlTag(tag);
+    }
+
+    @Override
     protected void onInitialize() {
         super.onInitialize();
         List<RdSideNavRecord<? extends IRequestablePage>> sideNavRecords = this.sideNavRecords;
 
+        add(RdSideNavBehavior.INSTANCE);
+
         add(
-            new WebMarkupContainer("sideNav") {
+            new WebMarkupContainer("sideNavList") {
                 @Override
                 protected void onInitialize() {
                     super.onInitialize();
-                    add(RdSideNavBehavior.INSTANCE);
 
+                    add(SIDE_NAV_LIST_BEHAVIOR); // TODO should be singleton
                     add(
-                        new WebMarkupContainer("sideNavList") {
+                        new ListView<>("sideNavItem", sideNavRecords) {
                             @Override
-                            protected void onInitialize() {
-                                super.onInitialize();
+                            protected void populateItem(
+                                ListItem<RdSideNavRecord<? extends IRequestablePage>> item
+                            ) {
+                                item.add(new RdSideNavItemBehavior()); // TODO: should be singleton
+                                RdSideNavRecord<? extends IRequestablePage> record =
+                                    item.getModelObject();
+                                var label = record.label();
 
-                                add(SIDE_NAV_LIST_BEHAVIOR); // TODO should be singleton
-                                add(
-                                    new ListView<>("sideNavItem", sideNavRecords) {
-                                        @Override
-                                        protected void populateItem(
-                                            ListItem<RdSideNavRecord<? extends IRequestablePage>> item
-                                        ) {
-                                            item.add(new RdSideNavItemBehavior()); // TODO: should be singleton
-                                            RdSideNavRecord<? extends IRequestablePage> record =
-                                                item.getModelObject();
-                                            var label = record.label();
-
-                                            @SuppressWarnings({ "rawtypes", "unchecked" })
-                                            RdSideNavLink<?> link = new RdSideNavLink(
-                                                "sideNavLink",
-                                                record.target()
-                                            );
-
-                                            // Optionally add an icon
-                                            WebMarkupContainer icon = new WebMarkupContainer("sideNavLinkIcon");
-                                            link.add(icon);
-                                            if (record.iconBehaviorSupplier() != null) {
-                                                icon.add(record.iconBehaviorSupplier().get());
-                                            } else {
-                                                icon.setVisible(false);
-                                            }
-
-                                            // Add the link text
-                                            link.add(new Label("sideNavLinkLabel", label));
-
-                                            // Optionally add a number badge
-                                            RdNumberBadge numberBadge = new RdNumberBadge(
-                                                "sideNavLinkNumberBadge",
-                                                record.numberBadge()
-                                            );
-                                            numberBadge.setVisible(record.numberBadge() >= 1);
-                                            link.add(numberBadge);
-
-                                            item.add(link);
-                                        }
-                                    }
+                                @SuppressWarnings({"rawtypes", "unchecked"})
+                                RdSideNavLink<?> link = new RdSideNavLink(
+                                    "sideNavLink",
+                                    record.target()
                                 );
+
+                                // Optionally add an icon
+                                WebMarkupContainer icon = new WebMarkupContainer("sideNavLinkIcon");
+                                link.add(icon);
+                                if (record.iconBehaviorSupplier() != null) {
+                                    icon.add(record.iconBehaviorSupplier().get());
+                                } else {
+                                    icon.setVisible(false);
+                                }
+
+                                // Add the link text
+                                link.add(new Label("sideNavLinkLabel", label));
+
+                                // Optionally add a number badge
+                                RdNumberBadge numberBadge = new RdNumberBadge(
+                                    "sideNavLinkNumberBadge",
+                                    record.numberBadge()
+                                );
+                                numberBadge.setVisible(record.numberBadge() >= 1);
+                                link.add(numberBadge);
+
+                                item.add(link);
                             }
                         }
                     );
