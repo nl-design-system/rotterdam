cc9ead206f5f1d69405e4ef8fbb51d38cb12d637
Kees van Dieren <kees@squins.com>
2025-12-15 10:21:24 +0100

    --textarea is non-existent, --text is used

--- rotterdam-nlds-parent-wicket/rotterdam-nlds-wicket/src/main/java/nl/rotterdam/nl_design_system/wicket/components/form_field_textarea/RdFormFieldTextAreaCss.java
+++ rotterdam-nlds-parent-wicket/rotterdam-nlds-wicket/src/main/java/nl/rotterdam/nl_design_system/wicket/components/form_field_textarea/RdFormFieldTextAreaCss.java
@@ -3,7 +3,9 @@ package nl.rotterdam.nl_design_system.wicket.components.form_field_textarea;
 import nl.rotterdam.nl_design_system.wicket.components.css_class_names.CssClassNames;
 
 enum RdFormFieldTextAreaCss implements CssClassNames {
-    BASE("utrecht-form-field--textarea");
+    // TODO this is shared between text box, textarea. But for form-field-checkbox, for form-field-radio it is different
+    // Discuss with Johan where to put these, in this enum, or move to RdFormFieldCss?
+    BASE("utrecht-form-field--text");
 
     private final String classNames;
 
