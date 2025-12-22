2c47bf0b3bb29b310e1b9abbde2d2f9eb8522d0d
Johan Stuyts <j.stuyts@rotterdam.nl>
2025-12-22 14:51:55 +0100

    #121 Form Summary verwijzen naar Data Summary.

--- rotterdam-nlds-parent-wicket/docs-wicket/src/main/java/nl/rotterdam/nl_design_system/docs/wicket/ComponentsPage.java
+++ rotterdam-nlds-parent-wicket/docs-wicket/src/main/java/nl/rotterdam/nl_design_system/docs/wicket/ComponentsPage.java
@@ -16,6 +16,7 @@ import nl.rotterdam.nl_design_system.docs.wicket.fieldset.FieldsetExamplesPanel;
 import nl.rotterdam.nl_design_system.docs.wicket.form_field_checkbox.FormFieldCheckboxExamplesPanel;
 import nl.rotterdam.nl_design_system.docs.wicket.form_field_label.FormFieldLabelExamplesPanel;
 import nl.rotterdam.nl_design_system.docs.wicket.form_field_text_input.FormFieldTextInputExamplesPanel;
+import nl.rotterdam.nl_design_system.docs.wicket.form_summary.FormSummaryExamplesPanel;
 import nl.rotterdam.nl_design_system.docs.wicket.heading.HeadingExamplesPanel;
 import nl.rotterdam.nl_design_system.docs.wicket.heading_group.HeadingGroupExamplesPanel;
 import nl.rotterdam.nl_design_system.docs.wicket.icon.IconExamplesPanel;
@@ -104,6 +105,7 @@ public class ComponentsPage extends RotterdamBasePage {
             new FormFieldCheckboxExamplesPanel("formFieldCheckboxExamplesPanel"),
             new FormFieldLabelExamplesPanel("formFieldLabelExamplesPanel"),
             new FormFieldTextInputExamplesPanel("formFieldTextInputExamplesPanel"),
+            new FormSummaryExamplesPanel("formSummaryExamplesPanel"),
             new DataSummaryExamplesPanel("dataSummaryExamplesPanel"),
             new HeadingExamplesPanel("headingExamplesPanel"),
             new HeadingGroupExamplesPanel("headingGroupExamplesPanel"),
---
+++ rotterdam-nlds-parent-wicket/rotterdam-nlds-wicket/src/main/java/nl/rotterdam/nl_design_system/wicket/components/form_summary/RdFormSummary.java
@@ -0,0 +1,17 @@
+package nl.rotterdam.nl_design_system.wicket.components.form_summary;
+
+/**
+ * <p>
+ * A <a href="https://nldesignsystem.nl/form-summary/">Form Summary</a>.
+ * </p>
+ * <p>
+ * Use {@link nl.rotterdam.nl_design_system.wicket.components.data_summary.RdDataSummary} instead as it uses the same
+ * implementation:
+ * the <a href="https://nl-design-system.github.io/utrecht/storybook/?path=/docs/css_css-data-list--docs">CSS
+ * implementation of Utrecht</a>.
+ * </p>
+ */
+public class RdFormSummary {
+    private RdFormSummary() {
+    }
+}
