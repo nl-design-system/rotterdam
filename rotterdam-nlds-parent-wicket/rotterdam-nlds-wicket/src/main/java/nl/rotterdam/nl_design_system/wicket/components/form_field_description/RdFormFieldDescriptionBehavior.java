32ba6ad296854c7e7c1cb900d502d1ca8d29241e
Johan Stuyts <j.stuyts@rotterdam.nl>
2025-12-22 11:58:52 +0100

    #125 code nagelopen. Documentatie uitgebreid.

--- rotterdam-nlds-parent-wicket/rotterdam-nlds-wicket/src/main/java/nl/rotterdam/nl_design_system/wicket/components/form_field_description/RdFormFieldDescriptionBehavior.java
+++ rotterdam-nlds-parent-wicket/rotterdam-nlds-wicket/src/main/java/nl/rotterdam/nl_design_system/wicket/components/form_field_description/RdFormFieldDescriptionBehavior.java
@@ -1,14 +1,26 @@
 package nl.rotterdam.nl_design_system.wicket.components.form_field_description;
 
 import nl.rotterdam.nl_design_system.wicket.components.base.TagNameClassComponentBehavior;
+import nl.rotterdam.nl_design_system.wicket.components.component_state.NlComponentState;
 import nl.rotterdam.nl_design_system.wicket.components.component_visibility.HideWhenModelIsNullBehavior;
 import org.apache.wicket.Component;
 
 import static nl.rotterdam.nl_design_system.wicket.components.CssReferenceHeaderItems.cssReferenceHeaderItem;
+import static nl.rotterdam.nl_design_system.wicket.components.component_state.Community.UTRECHT;
+import static nl.rotterdam.nl_design_system.wicket.components.component_state.EstafetteState.COMMUNITY;
+import static nl.rotterdam.nl_design_system.wicket.components.component_state.WicketState.BETA;
 
 /**
- * Apply Form field description behavior to an HTML element.
+ * <p>
+ * Turn an element into a <a href="https://nldesignsystem.nl/form-field-description/">Form Field Description</a>.
+ * </p>
+ * <p>
+ * Based on the
+ * <a href="https://nl-design-system.github.io/utrecht/storybook/?path=/docs/css_css-form-field-description--docs">CSS
+ * implementation of Utrecht</a>.
+ * </p>
  */
+@NlComponentState(wicketState = BETA, estafetteState = COMMUNITY, htmlCssImplementedBy = UTRECHT)
 public class RdFormFieldDescriptionBehavior extends TagNameClassComponentBehavior<Component> {
 
     /**
