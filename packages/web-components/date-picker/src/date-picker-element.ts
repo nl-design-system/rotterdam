a721f165eca9f585eac712d078a720c43fddbaf6
Robbert Broersma <git@robbertbroersma.nl>
2026-01-23 09:29:47 +0100

    fix: show correct date in mobile date picker

--- packages/web-components/date-picker/src/date-picker-element.ts
+++ packages/web-components/date-picker/src/date-picker-element.ts
@@ -324,8 +324,19 @@ export class DatePickerElement extends LitElement {
       return labels[n];
     };
 
-    const formattedSelectedDate = new Intl.DateTimeFormat(lang, { dateStyle: 'full' }).format(this._visibleDate);
-    const formattedSelectedTime = new Intl.DateTimeFormat(lang, { timeStyle: 'short' }).format(this._visibleDate);
+    const formattedSelectedDate = this._dateValue
+      ? new Intl.DateTimeFormat(lang, { dateStyle: 'full' }).format(this._dateValue)
+      : '';
+    let formattedSelectedTime = this._dateValue
+      ? new Intl.DateTimeFormat(lang, { timeStyle: 'short' }).format(this._dateValue)
+      : '';
+
+    if (hourSuffixLocal) {
+      formattedSelectedTime = formattedSelectedTime
+        ? `${formattedSelectedTime} ${hourSuffixLocal}`
+        : formattedSelectedTime;
+    }
+
     const formattedSelectedDateTime = this._dateValue
       ? new Intl.DateTimeFormat(lang, { dateStyle: 'full', timeStyle: 'short' }).format(this._dateValue)
       : '';
@@ -622,9 +633,7 @@ export class DatePickerElement extends LitElement {
                   />
                 </svg>
               </span>
-              <span class="utrecht-textbox__value" id="mobile-time-button-text"
-                >${formattedSelectedTime} ${hourSuffixLocal}</span
-              >
+              <span class="utrecht-textbox__value" id="mobile-time-button-text">${formattedSelectedTime}</span>
             </button>
           </div>
         </div>
@@ -632,7 +641,7 @@ export class DatePickerElement extends LitElement {
           <p>
             ${afspraakLocale}<br />
             <span>op <strong>${formattedSelectedDate}</strong></span>
-            <br /><span>om <strong>${formattedSelectedTime} ${hourSuffixLocal}</strong></span>
+            <br /><span>om <strong>${formattedSelectedTime}</strong></span>
           </p>
           <p>
             <button type="button" class="utrecht-button utrecht-button--secondary-action">${confirmLocale}</button>
