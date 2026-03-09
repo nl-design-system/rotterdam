42b2507dc413a1dce6c7c36cfa0858cd5d5d8c7c
Kees van Dieren <kees@squins.com>
2026-03-09 16:59:38 +0100

    Add example for available days, fixed existing example

--- rotterdam-nlds-parent-wicket/docs-wicket/src/main/java/nl/rotterdam/nl_design_system/docs/wicket/checkbox_group/CheckboxGroupExamplesPanel.java
+++ rotterdam-nlds-parent-wicket/docs-wicket/src/main/java/nl/rotterdam/nl_design_system/docs/wicket/checkbox_group/CheckboxGroupExamplesPanel.java
@@ -5,7 +5,8 @@ import nl.rotterdam.nl_design_system.docs.wicket.ExamplesPanel;
 import nl.rotterdam.nl_design_system.wicket.components.checkbox.RdCheckboxBehavior;
 import nl.rotterdam.nl_design_system.wicket.components.checkbox_group.RdCheckboxGroup;
 import nl.rotterdam.nl_design_system.wicket.components.form_field_label.RdFormFieldLabelBehavior;
-import nl.rotterdam.nl_design_system.wicket.components.form_field_label.RdFormFieldLabelCheckableInputType;
+import org.apache.wicket.ajax.form.AjaxFormChoiceComponentUpdatingBehavior;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Check;
 import org.apache.wicket.markup.html.list.ListItem;
@@ -20,6 +21,8 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+
+import static nl.rotterdam.nl_design_system.wicket.components.form_field_label.RdFormFieldLabelCheckableInputType.CHECKBOX;
 
 public class CheckboxGroupExamplesPanel extends ExamplesPanel {
     public CheckboxGroupExamplesPanel(String id) {
@@ -40,17 +43,36 @@ public class CheckboxGroupExamplesPanel extends ExamplesPanel {
             TELEGRAM
         }
 
+        IModel<Collection<ContactOption>> selectedModel = Model.of(List.of(ContactOption.PHONE));
+
         RdCheckboxGroup<ContactOption> component = new RdCheckboxGroup<>(
             "checkboxGroup",
-            Model.of(Collections.emptyList()),
+            selectedModel,
             new StringResourceModel("ContactOptionCaption"));
+
+        component.getCheckGroup()
+            .setOutputMarkupId(true)
+            .add(AjaxFormChoiceComponentUpdatingBehavior
+                .onUpdateChoice(target -> target.add(component.getCheckGroup()))
+            );
 
         component
             .add(
-                new Check<>("checkPhone", Model.of(ContactOption.PHONE)).add(RdCheckboxBehavior.INSTANCE),
-                new Check<>("checkEmail", Model.of(ContactOption.EMAIL)).add(RdCheckboxBehavior.INSTANCE),
-                new Check<>("checkWhatsapp", Model.of(ContactOption.WHATSAPP)).add(RdCheckboxBehavior.INSTANCE),
-                new Check<>("checkTelegram", Model.of(ContactOption.TELEGRAM)).add(RdCheckboxBehavior.INSTANCE)
+                new WebMarkupContainer("checkPhone")
+                    .add(new Check<>("check", Model.of(ContactOption.PHONE)).add(RdCheckboxBehavior.INSTANCE))
+                    .add(new RdFormFieldLabelBehavior(CHECKBOX, selectedModel.map(selected -> selected.contains(ContactOption.PHONE)))),
+
+                new WebMarkupContainer("checkEmail")
+                    .add(new Check<>("check", Model.of(ContactOption.EMAIL)).add(RdCheckboxBehavior.INSTANCE))
+                    .add(new RdFormFieldLabelBehavior(CHECKBOX, selectedModel.map(selected -> selected.contains(ContactOption.EMAIL)))),
+
+                new WebMarkupContainer("checkWhatsapp")
+                    .add(new Check<>("check", Model.of(ContactOption.WHATSAPP)).add(RdCheckboxBehavior.INSTANCE))
+                    .add(new RdFormFieldLabelBehavior(CHECKBOX, selectedModel.map(selected -> selected.contains(ContactOption.WHATSAPP)))),
+
+                new WebMarkupContainer("checkTelegram")
+                    .add(new Check<>("check", Model.of(ContactOption.TELEGRAM)).add(RdCheckboxBehavior.INSTANCE))
+                    .add(new RdFormFieldLabelBehavior(CHECKBOX, selectedModel.map(selected -> selected.contains(ContactOption.TELEGRAM))))
             );
 
         return component;
@@ -59,32 +81,8 @@ public class CheckboxGroupExamplesPanel extends ExamplesPanel {
     @ComponentExample
     public static RdCheckboxGroup<DayOfWeek> checkboxGroupSelectDays() {
         IModel<Collection<DayOfWeek>> selectedItems = Model.of(List.of(DayOfWeek.MONDAY, DayOfWeek.THURSDAY));
-        RdCheckboxGroup<DayOfWeek> component = new RdCheckboxGroup<>(
-            "checkboxGroupSelectDays",
-            selectedItems,
-            new StringResourceModel("ContactOptionCaption"));
 
-        component.add(new ListView<>("day", Arrays.asList(DayOfWeek.values())) {
-            @Override
-            protected void populateItem(ListItem<DayOfWeek> item) {
-
-                IModel<String> labelModel = item.
-                    getModel().map(day -> day.getDisplayName(TextStyle.FULL, getSession().getLocale()));
-
-                item.add(
-                    new Check<>("checkbox", item.getModel())
-                        .setLabel(labelModel)
-                        .add(RdCheckboxBehavior.INSTANCE),
-
-                    new Label("label", labelModel)
-                );
-
-                item.add(new RdFormFieldLabelBehavior(RdFormFieldLabelCheckableInputType.CHECKBOX,
-                    selectedItems.map(selected -> selected.contains(item.getModelObject()))));
-            }
-        });
-
-        return component;
+        return new DayOfWeekCheckboxGroup("checkboxGroupSelectDays", selectedItems, new StringResourceModel("AvailableDaysOptionCaption"));
     }
 
     @ComponentExample
@@ -120,7 +118,7 @@ public class CheckboxGroupExamplesPanel extends ExamplesPanel {
     @Override
     protected void onInitialize() {
         super.onInitialize();
-        
+
         add(
             exampleCheckboxGroup(),
             checkboxGroupSelectDays(),
@@ -128,5 +126,51 @@ public class CheckboxGroupExamplesPanel extends ExamplesPanel {
             exampleCheckboxGroupDisabled(),
             exampleCheckboxGroupInvalid()
         );
+    }
+
+    private static class DayOfWeekCheckboxGroup extends RdCheckboxGroup<DayOfWeek> {
+
+        private final IModel<Collection<DayOfWeek>> selectedItems;
+
+        private DayOfWeekCheckboxGroup(String id, IModel<Collection<DayOfWeek>> selectedItems, StringResourceModel labelModel) {
+            super(id, selectedItems, labelModel);
+            this.selectedItems = selectedItems;
+        }
+
+        @Override
+        protected void onInitialize() {
+            super.onInitialize();
+
+            getCheckGroup()
+                .setOutputMarkupId(true)
+                .add(AjaxFormChoiceComponentUpdatingBehavior
+                    .onUpdateChoice(target -> target.add(getCheckGroup()))
+                );
+
+            add(new ListView<>("day", Arrays.asList(DayOfWeek.values())) {
+                {
+                    setReuseItems(true);
+                }
+
+                @Override
+                protected void populateItem(ListItem<DayOfWeek> item) {
+
+                    IModel<String> labelModel = item.
+                        getModel().map(day -> day.getDisplayName(TextStyle.FULL, getSession().getLocale()));
+
+                    item.add(
+                        new Check<>("checkbox", item.getModel())
+                            .setLabel(labelModel)
+                            .add(RdCheckboxBehavior.INSTANCE),
+
+                        new Label("label", labelModel)
+                    );
+
+                    item.add(new RdFormFieldLabelBehavior(CHECKBOX,
+                        selectedItems.map(selected -> selected.contains(item.getModelObject()))));
+                }
+            });
+
+        }
     }
 }
