0cc737abafd036f986db9e65d82a22b85d940ead
Kees van Dieren <kees@squins.com>
2026-03-09 15:25:02 +0100

    Add example to select days

--- rotterdam-nlds-parent-wicket/docs-wicket/src/main/java/nl/rotterdam/nl_design_system/docs/wicket/checkbox_group/CheckboxGroupExamplesPanel.java
+++ rotterdam-nlds-parent-wicket/docs-wicket/src/main/java/nl/rotterdam/nl_design_system/docs/wicket/checkbox_group/CheckboxGroupExamplesPanel.java
@@ -4,11 +4,22 @@ import nl.rotterdam.nl_design_system.docs.wicket.ComponentExample;
 import nl.rotterdam.nl_design_system.docs.wicket.ExamplesPanel;
 import nl.rotterdam.nl_design_system.wicket.components.checkbox.RdCheckboxBehavior;
 import nl.rotterdam.nl_design_system.wicket.components.checkbox_group.RdCheckboxGroup;
+import nl.rotterdam.nl_design_system.wicket.components.form_field_label.RdFormFieldLabelBehavior;
+import nl.rotterdam.nl_design_system.wicket.components.form_field_label.RdFormFieldLabelCheckableInputType;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Check;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.StringResourceModel;
 
+import java.time.DayOfWeek;
+import java.time.format.TextStyle;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 public class CheckboxGroupExamplesPanel extends ExamplesPanel {
     public CheckboxGroupExamplesPanel(String id) {
@@ -45,7 +56,36 @@ public class CheckboxGroupExamplesPanel extends ExamplesPanel {
         return component;
     }
 
+    @ComponentExample
+    public static RdCheckboxGroup<DayOfWeek> checkboxGroupSelectDays() {
+        IModel<Collection<DayOfWeek>> selectedItems = Model.of(List.of(DayOfWeek.MONDAY, DayOfWeek.THURSDAY));
+        RdCheckboxGroup<DayOfWeek> component = new RdCheckboxGroup<>(
+            "checkboxGroupSelectDays",
+            selectedItems,
+            new StringResourceModel("ContactOptionCaption"));
 
+        component.add(new ListView<>("day", Arrays.asList(DayOfWeek.values())) {
+            @Override
+            protected void populateItem(ListItem<DayOfWeek> item) {
+
+                IModel<String> labelModel = item.
+                    getModel().map(day -> day.getDisplayName(TextStyle.FULL, getSession().getLocale()));
+
+                item.add(
+                    new Check<>("checkbox", item.getModel())
+                        .setLabel(labelModel)
+                        .add(RdCheckboxBehavior.INSTANCE),
+
+                    new Label("label", labelModel)
+                );
+
+                item.add(new RdFormFieldLabelBehavior(RdFormFieldLabelCheckableInputType.CHECKBOX,
+                    selectedItems.map(selected -> selected.contains(item.getModelObject()))));
+            }
+        });
+
+        return component;
+    }
 
     @ComponentExample
     private static RdCheckboxGroup<String> exampleCheckboxGroupWithDescription() {
@@ -63,6 +103,7 @@ public class CheckboxGroupExamplesPanel extends ExamplesPanel {
             Model.of(Collections.<String>emptyList()),
             new StringResourceModel("TheCaption"));
         component.setEnabled(false);
+
         return component;
     }
 
@@ -80,6 +121,12 @@ public class CheckboxGroupExamplesPanel extends ExamplesPanel {
     protected void onInitialize() {
         super.onInitialize();
         
-        add(exampleCheckboxGroup(), exampleCheckboxGroupWithDescription(), exampleCheckboxGroupDisabled(), exampleCheckboxGroupInvalid());
+        add(
+            exampleCheckboxGroup(),
+            checkboxGroupSelectDays(),
+            exampleCheckboxGroupWithDescription(),
+            exampleCheckboxGroupDisabled(),
+            exampleCheckboxGroupInvalid()
+        );
     }
 }
